03b82898afd061d6ae5fa7240b292d76f2ee762c
antonnell <antonnell.crpto@gmail.com>
2021-03-25 10:40:00 +0200

    Bump version number.
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ function Home(props) {
                 </svg>
                 <Typography variant='subtitle1' className={ classes.sourceCode }>View Source Code</Typography>
               </a>
-              <Typography variant='subtitle1' className={ classes.version }>Version 1.0.0</Typography>
+              <Typography variant='subtitle1' className={ classes.version }>Version 1.0.1</Typography>
             </div>
           </div>
           <Grid container spacing={4} className={ classes.listContainer }>
