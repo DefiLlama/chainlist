d0b5392dad72644b6b666ac83ad75cd0383ac03c
Danny <danny@position.exchange>
2022-08-18 21:50:13 +0700

    Only need posichain mainnet

--- constants/chainIds.js
+++ constants/chainIds.js
@@ -55,7 +55,6 @@ const chainIds = {
   9001: "evmos",
   10000: "smartbch",
   900000: "posichain",
-  910000: "posichain-testnet",
   103090: "crystaleum",
   32659: "fusion",
   42161: "arbitrum",
