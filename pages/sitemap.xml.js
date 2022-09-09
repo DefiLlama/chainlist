cb59cc1013d0561e8c55248a6be136469165800a
mintdart <96025197+mintdart@users.noreply.github.com>
2022-09-09 14:01:48 -0400

    update sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,6 @@
-const EXTERNAL_DATA_URL = 'https://chainid.network/chains.json';
+import chainIds from "../constants/chainIds";
+
+const EXTERNAL_DATA_URL = "https://chainid.network/chains.json";
 
 function generateSiteMap(chains) {
   return `<?xml version="1.0" encoding="UTF-8"?>
@@ -15,7 +17,25 @@ function generateSiteMap(chains) {
        </url>
      `;
        })
-       .join('')}
+       .join("")}
+     ${chains
+       .map(({ name }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${name.toLowerCase().split(" ").join("%20")}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+     ${Object.values(chainIds)
+       .map((name) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${name}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
    </urlset>
  `;
 }
@@ -24,23 +44,22 @@ function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
-
 export async function getServerSideProps({ res }) {
-    // We make an API call to gather the URLs for our site
-    const request = await fetch(EXTERNAL_DATA_URL);
-    const chains = await request.json();
-  
-    // We generate the XML sitemap with the chains data
-    const sitemap = generateSiteMap(chains);
-  
-    res.setHeader('Content-Type', 'text/xml');
-    // we send the XML to the browser
-    res.write(sitemap);
-    res.end();
-  
-    return {
-      props: {},
-    };
-  }
-  
-  export default SiteMap;
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const chains = await request.json();
+
+  // We generate the XML sitemap with the chains data
+  const sitemap = generateSiteMap(chains);
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
