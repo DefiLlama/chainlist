9df77d574fd1b248d1f4c868670d4df893b93583
brenopolanski <breno.polanski@gmail.com>
2022-12-22 17:09:27 -0300

    chore: update privacy info for llamanodes

--- constants/llamaNodesRpcs.js
+++ constants/llamaNodesRpcs.js
@@ -1,4 +1,4 @@
-const privacyStatement = 'LlamaNodes does not collect, log, or share user data that transits through our RPCs, ever. LlamaNodes does not collect the IP addresses of your users. LlamaNodes does not collect the wallet addresses of your users. https://llamanodes.notion.site/Privacy-Practices-f20fd8fdd02a469d9d4f42a5989bb936';
+const privacyStatement = 'LlamaNodes is open-source and does track or store any kind of user information (i.e., location, IP, wallet address, etc) that transits through our RPCs, ever. To learn more, review the public privacy policy: https://llamanodes.notion.site/Privacy-Practices-f20fd8fdd02a469d9d4f42a5989bb936';
 
 export const llamaNodesRpcs = {
   1: {
