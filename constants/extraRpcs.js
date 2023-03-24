23e1cf8e2a06a71bf0b8cd055c28611d9cd2a1bb
realdealshaman <realdealshaman11@gmail.com>
2023-03-24 17:44:56 +0000

    add privacy policy

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -58,7 +58,9 @@ const privacyStatement = {
   getblock:
     "We automatically collect certain information through cookies and similar technologies when you visit, use or navigate Website. This information does not reveal your specific identity (like your name or contact information) and does not allow to identify you. However, it may include device and usage information, such as your IP address, browser and device characteristics, its type and version, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our Website, information about your interaction in our emails, and other technical and statistical information. This information is primarily needed to maintain the security and operation of our Website, and for our internal analytics and reporting purposes.Specifically, as the RPC provider, we do not log and store your IP address, country, location and similar data. https://getblock.io/privacy-policy/",
   teamblockchain:
-    "We may collect non-personal information such as your browser type, type of computer, operating system, IP address, or other minor details. https://policy.teamblockchain.team/"
+    "We may collect non-personal information such as your browser type, type of computer, operating system, IP address, or other minor details. https://policy.teamblockchain.team/",
+  getloop:
+    "Loop Network follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. https://www.getloop.network/privacypolicy"
 };
 
 export const extraRpcs = {
@@ -2136,7 +2138,13 @@ export const extraRpcs = {
     ],
   },
   15551: {
-    rpcs: ["https://api.mainnetloop.com"],
+    rpcs: [
+      {
+        url: "https://api.mainnetloop.com",
+        tracking: "limited",
+        trackingDetails: privacyStatement.getloop
+      }
+    ],
   },
   88888888: {
     rpcs: [
