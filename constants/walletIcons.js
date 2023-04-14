e13dbb3a61c5a99e4116e90af31dae95a4b8dd0d
Daedalus <0xDaedalus@users.noreply.github.com>
2023-04-14 07:31:49 -0600

    Add Taho Connector

--- constants/walletIcons.js
+++ constants/walletIcons.js
@@ -1,5 +1,6 @@
 export const walletIcons = {
   "Coinbase Wallet": "/connectors/coinbaseWalletIcon.svg",
+  Taho: "/connectors/icn-taho.svg",
   "Brave Wallet": "/connectors/icn-bravewallet.svg",
   Metamask: "/connectors/icn-metamask.svg",
   imToken: "/connectors/icn-imtoken.svg",
