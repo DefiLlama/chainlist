ceae06e7f1fd0ffc8b66f4daa9a1733490ef5bc2
brenopolanski <breno.polanski@gmail.com>
2023-04-19 16:09:47 -0300

    feat: add handle functions

--- components/adbanner/index.js
+++ components/adbanner/index.js
@@ -1,12 +1,32 @@
+import { useState } from 'react'
+
+const BANNERS = [
+  {
+    url: './ads/llamanodes-banner.png'
+  },
+  {
+    url: './ads/gmx-banner.png'
+  }
+]
+
 export const AdBanner = () => {
-  const banners = [
-    {
-      url: './ads/llamanodes-banner.png'
-    },
-    {
-      url: './ads/gmx-banner.png'
-    }
-  ]
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handlePrevBanner = () => {
+    const isFirstBanner = currentIndex === 0
+    const newIndex = isFirstBanner ? BANNERS.length - 1 : currentIndex - 1
+
+    setCurrentIndex(newIndex)
+  }
+
+  const handleNextBanner = () => {
+    const isLastBanner = currentIndex === BANNERS.length - 1
+    const newIndex = isLastBanner ? 0 : currentIndex + 1
+
+    setCurrentIndex(newIndex)
+  }
+
+  const handleGoToBanner = (bannerIndex) => setCurrentIndex(bannerIndex)
 
   return (
     <div className="shadow dark:bg-[#0D0D0D] bg-white p-8 pb-0 rounded-[10px] flex flex-col gap-3 overflow-hidden">
