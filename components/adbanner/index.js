2641868bba45bbbe8e24258dff8060742391cf8c
brenopolanski <breno.polanski@gmail.com>
2023-04-19 16:02:07 -0300

    feat: add init code to AdBanner component

---
+++ components/adbanner/index.js
@@ -0,0 +1,15 @@
+export const AdBanner = () => {
+  const banners = [
+    {
+      url: './ads/llamanodes-banner.png'
+    },
+    {
+      url: './ads/gmx-banner.png'
+    }
+  ]
+
+  return (
+    <div className="shadow dark:bg-[#0D0D0D] bg-white p-8 pb-0 rounded-[10px] flex flex-col gap-3 overflow-hidden">
+    </div>
+  )
+}
