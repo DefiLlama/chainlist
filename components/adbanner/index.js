3c62c1c613b155e3ca6fee5ae12f9fd1772441bb
brenopolanski <breno.polanski@gmail.com>
2023-04-19 21:09:57 -0300

    feat: set the interval duration in miliseconds when the carousel is cycling

--- components/adbanner/index.js
+++ components/adbanner/index.js
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import { notTranslation as useTranslations } from '../../utils'
 
 const BANNERS = [
   {
@@ -10,7 +12,15 @@ const BANNERS = [
 ]
 
 export const AdBanner = () => {
+  const t = useTranslations('Common')
+
   const [currentIndex, setCurrentIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => handleNextBanner(), 5000)
+
+    return () => clearInterval(interval)
+  }, [currentIndex])
 
   const handlePrevBanner = () => {
     const isFirstBanner = currentIndex === 0
@@ -27,20 +37,30 @@ export const AdBanner = () => {
   }
 
   return (
-    <div className="w-full h-full m-auto relative group">
+    <div className="shadow w-full h-full m-auto relative group rounded-[10px]">
       <div
         style={{ backgroundImage: `url(${BANNERS[currentIndex].url})` }}
         className="w-full h-full rounded-[10px] bg-center bg-cover duration-500"
       />
 
       {/* left arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <div onClick={handlePrevBanner}>{'<'}</div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handlePrevBanner}>
+        &lsaquo;
       </div>
 
       {/* right arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <div onClick={handleNextBanner}>{'>'}</div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handleNextBanner}>
+        &rsaquo;
+      </div>
+
+      <div className="w-full absolute bottom-0 text-center dark:bg-[#0D0D0D] bg-white rounded-b-[8px] text-xs text-gray-500 dark:text-[#B3B3B3] py-0.5">
+        <a
+          href="https://github.com/DefiLlama/chainlist"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {t('your-ad-here')}, {t('contact-us').toLowerCase()}
+        </a>
       </div>
     </div>
   )
