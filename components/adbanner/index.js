c3c54ff76c55d8f678ce6d38fe8aa92c50ed213b
brenopolanski <breno.polanski@gmail.com>
2023-04-19 21:16:25 -0300

    feat: add props

--- components/adbanner/index.js
+++ components/adbanner/index.js
@@ -4,22 +4,22 @@ import { notTranslation as useTranslations } from '../../utils'
 
 const BANNERS = [
   {
-    url: './ads/llamanodes-banner.png'
+    image: './ads/llamanodes-banner.png'
   },
   {
-    url: './ads/gmx-banner.png'
+    image: './ads/gmx-banner.png'
   }
 ]
 
-export const AdBanner = () => {
+export const AdBanner = ({ timer = 5000, showControls = false }) => {
   const t = useTranslations('Common')
 
   const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
-    const interval = setInterval(() => handleNextBanner(), 5000)
+    const intervalId = setInterval(() => handleNextBanner(), timer)
 
-    return () => clearInterval(interval)
+    return () => clearInterval(intervalId)
   }, [currentIndex])
 
   const handlePrevBanner = () => {
@@ -39,19 +39,23 @@ export const AdBanner = () => {
   return (
     <div className="shadow w-full h-full m-auto relative group rounded-[10px]">
       <div
-        style={{ backgroundImage: `url(${BANNERS[currentIndex].url})` }}
+        style={{ backgroundImage: `url(${BANNERS[currentIndex].image})` }}
         className="w-full h-full rounded-[10px] bg-center bg-cover duration-500"
       />
 
       {/* left arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handlePrevBanner}>
-        &lsaquo;
-      </div>
+      {showControls && (
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handlePrevBanner}>
+          &lsaquo;
+        </div>
+      )}
 
       {/* right arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handleNextBanner}>
-        &rsaquo;
-      </div>
+      {showControls && (
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer" onClick={handleNextBanner}>
+          &rsaquo;
+        </div>
+      )}
 
       <div className="w-full absolute bottom-0 text-center dark:bg-[#0D0D0D] bg-white rounded-b-[8px] text-xs text-gray-500 dark:text-[#B3B3B3] py-0.5">
         <a
