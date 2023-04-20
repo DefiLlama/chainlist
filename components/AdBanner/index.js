f9080fcc3d85cbbda6144c816616316f7df549e3
brenopolanski <breno.polanski@gmail.com>
2023-04-20 17:02:27 -0300

    wip

--- components/AdBanner/index.js
+++ components/AdBanner/index.js
@@ -11,7 +11,7 @@ const BANNERS = [
     url: 'https://llamanodes.com',
   },
   {
-    image: './banners/llamanodes-banner.png',
+    image: './banners/gmx-banner.png',
     name: 'GMX',
     url: 'https://app.gmx.io/#/trade/?ref=chainlist',
   },
@@ -51,43 +51,45 @@ export const AdBanner = ({ timer = 15000, startTransition = true, showControls =
   }
 
   return (
-    <div className="shadow w-full h-auto m-auto relative group rounded-[10px]">
-      <a
-        href={randomBanners[currentIndex].url}
-        rel="noopener noreferrer"
-        target="_blank"
-        onClick={() => Fathom.trackGoal(FATHOM_ADS_ID[randomBanners[currentIndex].name.toLowerCase()], 0)}
-      >
-        <img
-          alt={`${randomBanners[currentIndex].name} banner`}
-          className="rounded-[10px] duration-500 w-full h-auto"
-          src={randomBanners[currentIndex].image}
-        />
-      </a>
-
-      {/* left arrow */}
-      {showControls && (
-        <div
-          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer"
-          onClick={handlePrevBanner}
+    <div className="flex flex-col w-full h-full justify-center gap-2">
+      <div className="w-full h-full relative group rounded-[10px]">
+        <a
+          href={randomBanners[currentIndex].url}
+          rel="noopener noreferrer"
+          target="_blank"
+          onClick={() => Fathom.trackGoal(FATHOM_ADS_ID[randomBanners[currentIndex].name.toLowerCase()], 0)}
         >
-          &lsaquo;
-        </div>
-      )}
+          <img
+            alt={`${randomBanners[currentIndex].name} banner`}
+            className="rounded-[10px] duration-500 w-full h-full"
+            src={randomBanners[currentIndex].image}
+          />
+        </a>
 
-      {/* right arrow */}
-      {showControls && (
-        <div
-          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer"
-          onClick={handleNextBanner}
-        >
-          &rsaquo;
-        </div>
-      )}
+        {/* left arrow */}
+        {showControls && (
+          <div
+            className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer"
+            onClick={handlePrevBanner}
+          >
+            &lsaquo;
+          </div>
+        )}
 
-      <div className="w-full absolute bottom-0 text-center dark:bg-[#0D0D0D] bg-white rounded-b-[8px] text-[11px] text-gray-500 dark:text-[#B3B3B3] italic">
+        {/* right arrow */}
+        {showControls && (
+          <div
+            className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-2 text-xl rounded-full px-2 bg-black/20 text-white cursor-pointer"
+            onClick={handleNextBanner}
+          >
+            &rsaquo;
+          </div>
+        )}
+      </div>
+
+      <div className="w-full text-center text-xs text-gray-500 dark:text-[#B3B3B3] italic">
         <a href="mailto:contact@llama-corp.com" rel="noopener noreferrer" target="_blank">
-          {t('your-ad-here')}, {t('contact-us').toLowerCase()}
+          {`${t('your-ad-here')}, ${t('contact-us').toLowerCase()}`}
         </a>
       </div>
     </div>
