1d1e058bbc90dac06a7253326c52641562992aa0
hpusdtki <132435412+hpusdtki@users.noreply.github.com>
2023-07-19 17:15:01 +0800

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -278,6 +278,11 @@ export const extraRpcs = {
         tracking: "yes",
         trackingDetails: privacyStatement.tenderly,
       },
+         {
+        url: "https://rpc.tenderly.co/fork/c63af728-a183-4cfb-b24e-a92801463484",
+        tracking: "yes",
+        trackingDetails: privacystatement.tenderly,
+      },
       {
       	url: "https://api.zan.top/node/v1/eth/mainnet/public",
       	tracking: "limited",
