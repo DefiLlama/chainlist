60e3a8c05d70e00648fb28b717373dda67b63070
0xngmi <0xngmi@protonmail.com>
2023-07-25 00:19:54 +0100

    fix build

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -278,7 +278,7 @@ export const extraRpcs = {
        {
         url: "https://rpc.tenderly.co/fork/c63af728-a183-4cfb-b24e-a92801463484",
         tracking: "yes",
-        trackingDetails: privacystatement.tenderly,
+        trackingDetails: privacyStatement.tenderly,
       },
       {
         url: "https://gateway.tenderly.co/public/mainnet",
