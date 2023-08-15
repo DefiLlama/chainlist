0ab50f4b276ddf94549b4742afdee0c697bf760c
Ed Weng <edweng@gmail.com>
2023-08-14 23:22:14 -0400

    Adding HypeLab SDK for programmatic banner advertising

--- components/AdBanner/index.js
+++ components/AdBanner/index.js
@@ -1,93 +1,93 @@
-import * as Fathom from "fathom-client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { FATHOM_ADS_ID } from "../../hooks/useAnalytics";
-import { notTranslation as useTranslations, shuffleArray } from "../../utils";
-
-const BANNERS = [
-  {
-    image: "brave.png",
-    name: "Brave",
-    url: "https://brave.com/wallet/?mtm_source=chainlist&mtm_medium=paid&mtm_campaign=q3wallet",
-    isActive: true,
-  },
-  {
-    image: "llamanodes.png",
-    name: "LlamaNodes",
-    url: "https://llamanodes.com",
-    isActive: true,
-  },
-  {
-    image: "gmx.png",
-    name: "GMX",
-    url: "https://app.gmx.io/#/trade/?ref=chainlist",
-    isActive: false,
-  },
-].filter((banner) => banner.isActive);
+import { Native, NativeMediaContent, NativeLink } from "hypelab-react";
+import { HYPELAB_NATIVE_PLACEMENT_SLUG } from "../../constants/hypelab";
 
 export const AdBanner = () => {
-  const t = useTranslations("Common");
-  const [banner, setBanner] = useState();
+  return (
+    <Native placement={HYPELAB_NATIVE_PLACEMENT_SLUG} className="h-full">
+      {function (ad) {
+        if (ad.icon !== "") {
+          return (
+            <div className="flex flex-col h-full shadow dark:bg-[#0D0D0D] bg-white rounded-[10px] flex flex-col overflow-hidden h-full">
+              <div className="flex">
+                <NativeLink>
+                  <div data-cy="mediaContent" className="mediaContent">
+                    <NativeMediaContent />
+                  </div>
+                </NativeLink>
+              </div>
+              <NativeTextContent ad={ad} />
+            </div>
+          );
+        }
+      }}
+    </Native>
+  );
+};
 
-  // only render banner client-side to prevent hydration errors due to the random banners
+const NativeTextContent = ({ ad }) => {
+  const containerRef = useRef(null);
+  const [width, setWidth] = useState(0);
+
   useEffect(() => {
-    setBanner(BANNERS[Math.floor(Math.random() * BANNERS.length)]);
+    const updateWidth = () => {
+      if (containerRef.current) {
+        setWidth(containerRef.current.clientWidth);
+      }
+    };
+
+    updateWidth(); // Initial width
+    window.addEventListener("resize", updateWidth);
+
+    return () => {
+      window.removeEventListener("resize", updateWidth);
+    };
   }, []);
 
-  if (!banner) {
-    return null;
-  }
-
-  const srcLarge = `./banners/large/${banner.image}`;
-  const srcSmall = `./banners/small/${banner.image}`;
-  const srcName = banner.name;
-
   return (
-    <div className="flex flex-col w-full h-full justify-center gap-2">
-      <div className="w-full h-full rounded-[10px]">
-        <a
-          href={banner.url}
-          rel="noopener noreferrer"
-          target="_blank"
-          onClick={() => Fathom.trackGoal(FATHOM_ADS_ID[banner.name.toLowerCase()], 0)}
-        >
-          <picture className="rounded-[10px] duration-500 w-full h-full">
-            <source
-              srcSet={srcSmall}
-              media="(max-width: 420px)"
-              className="rounded-[10px] duration-500 w-full h-full"
-            />
-            <source
-              srcSet={srcLarge}
-              media="(max-width: 639px)"
-              className="rounded-[10px] duration-500 w-full h-full"
-            />
-            <source
-              srcSet={srcSmall}
-              media="(max-width: 1399px)"
-              className="rounded-[10px] duration-500 w-full h-full"
-            />
-            <source
-              srcSet={srcLarge}
-              media="(max-width: 1679px)"
-              className="rounded-[10px] duration-500 w-full h-full"
-            />
-            <source
-              srcSet={srcSmall}
-              media="(max-width: 2000px)"
-              className="rounded-[10px] duration-500 w-full h-full"
-            />
-
-            <img src={srcLarge} alt={srcName} className="rounded-[10px] duration-500 w-full h-full" />
-          </picture>
-        </a>
-      </div>
-
-      <div className="w-full text-center text-xs text-gray-500 dark:text-[#B3B3B3] italic">
-        <a href="mailto:contact@llama-corp.com" rel="noopener noreferrer" target="_blank">
-          {`${t("your-ad-here")}, ${t("contact-us").toLowerCase()}`}
-        </a>
+    <div className="flex flex-col items-center justify-center mx-3 my-3 h-full" id="ctaContainer" ref={containerRef}>
+      <div className="flex flex-col items-center justify-center w-full gap-2">
+        {width < 370 ? (
+          <>
+            <AdvertiserInfo ad={ad} />
+            <CtaButton ad={ad} />
+          </>
+        ) : (
+          <div className="flex justify-between w-full">
+            <AdvertiserInfo ad={ad} />
+            <CtaButton ad={ad} />
+          </div>
+        )}
       </div>
     </div>
+  );
+};
+
+const AdvertiserInfo = ({ ad }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="rounded-full flex-shrink-0 flex relative">
+        <NativeLink>
+          <img src={ad.icon} className="w-10 rounded-full" alt={ad.icon} />
+        </NativeLink>
+      </div>
+      <span className="font-semibold whitespace-nowrap overflow-hidden text-ellipsis relative top-[1px] dark:text-[#B3B3B3]">
+        <NativeLink>{ad.advertiser}</NativeLink>
+      </span>
+    </div>
+  );
+};
+
+const CtaButton = ({ ad }) => {
+  return (
+    <NativeLink>
+      <div
+        data-cy="ctaText"
+        className="border dark:border-[#171717] border-[#EAEAEA] text-center px-4 py-2 rounded-[50px] dark:text-[#2F80ED] text-[#2F80ED] dark:hover:text-black hover:text-white dark:hover:bg-[#2F80ED] hover:bg-[#2F80ED] truncate"
+      >
+        {ad.ctaText}
+      </div>
+    </NativeLink>
   );
 };
