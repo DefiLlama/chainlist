addd07349e998fd3776d1439ee155c2a590615ef
swarna1101 <swarnabhasinha@gmail.com>
2023-09-09 03:48:46 +0530

    Add Shardeum Testnet RPCs (#685)
    * Add Shardeum Testnet RPCs
    
    * fix: privacy details and RPC specs
    
    * fix: privacy tracking for the RPCs
    
    * fix: privacy tracking status
    
    ---------
    
    Co-authored-by: Swarnabha Sinha <swarnabhasinha.201mn059@nitk.edu.in>

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -2798,6 +2798,16 @@ export const extraRpcs = {
         trackingDetails: privacyStatement.tenderly,
       },
       {
+        url: "https://sphinx.shardeum.org/",
+        tracking: "yes",
+        trackingDetails: privacyStatement.shardeum,
+      },
+      {
+        url: "https://dapps.shardeum.org/",
+        tracking: "yes",
+        trackingDetails: privacyStatement.shardeum,
+      },
+      {
       	url: "https://api.zan.top/node/v1/eth/sepolia/public",
       	tracking: "limited",
       	trackingDetails: privacyStatement.zan,
@@ -3282,6 +3292,24 @@ export const extraRpcs = {
         url: "https://syscoin-tanenbaum-evm.publicnode.com",
         tracking: "none",
         trackingDetails: privacyStatement.publicnode,
+      }
+    ],
+  },
+  8081: {
+    rpcs: [
+      {
+        url: "https://dapps.shardeum.org/",
+        tracking: "yes",
+        trackingDetails: privacyStatement.shardeum,
+      }
+    ],
+  },
+  8082: {
+    rpcs: [
+      {
+        url: "https://sphinx.shardeum.org/",
+        tracking: "yes",
+        trackingDetails: privacyStatement.shardeum,
       }
     ],
   },
