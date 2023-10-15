603567a8e9c4c9c373648f6dfdbc34b1917c1de1
Rana hayat <143619494+Ranahayat1122@users.noreply.github.com>
2023-10-16 02:20:27 +0500

    Update walletIcons

--- constants/walletIcons.js
+++ constants/walletIcons.js
@@ -1,9 +1,9 @@
-export const walletIcons = {
-  "Coinbase Wallet": "/connectors/coinbaseWalletIcon.svg",
-  Taho: "/connectors/icn-taho.svg",
-  "Brave Wallet": "/connectors/icn-bravewallet.svg",
-  Metamask: "/connectors/icn-metamask.svg",
-  imToken: "/connectors/icn-imtoken.svg",
-  Wallet: "/connectors/icn-metamask.svg",
-  "Trust Wallet": "/connectors/icon-trust.svg",
-};
+export const walletIcons = {0x564ef37e0104426e829d66dc916a30376ae1239c
+  "trustwallet": "/connectors/trustwalleticon.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  trust: "/connectors/icn-trustwallet.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  "trust Wallet": "/connectors/icn-trustwallet.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  trustwallet: "/connectors/icn-trustwallet.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  trustwallet: "/connectors/icn-trustwallet.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  Wallet: "/connectors/icn-trustwallet.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+  "Trust Wallet": "/connectors/icon-trust.svg",0x564ef37e0104426e829d66dc916a30376ae1239c
+};0x564ef37e0104426e829d66dc916a30376ae1239c
