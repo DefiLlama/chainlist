20ba3af4a59e418f3a58f0c8ea887fd297b42944
0xx92 <133748557+0xx92@users.noreply.github.com>
2023-10-17 02:10:34 +0800

    Update extraRpcs.js (#730)

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -3029,6 +3029,13 @@ export const extraRpcs = {
   281121: {
     rpcs: ["rpcWorking:false"],
   },
+  534352: {
+    rpcs: [
+      {
+        url: "https://scroll.blockpi.network/v1/rpc/public",
+        tracking: "limited",
+        trackingDetails: privacyStatement.blockpi,
+      },
   534353: {
     rpcs: [
       {
