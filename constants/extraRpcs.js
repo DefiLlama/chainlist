fd3362e8d37eca49dc89d8e3e3c2878c502d9226
Mohit <59959710+mohitdhiman3145@users.noreply.github.com>
2023-10-19 22:31:00 +0530

    Update extraRpcs.js
    change in the SSL certificate of RPC

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1927,7 +1927,7 @@ export const extraRpcs = {
       "https://rpc.xinfin.network",
       "https://rpc-xdc.icecreamswap.com",
       "https://erpc.xdcrpc.com"
-      "http://rpc.xdc.org"
+      "https://rpc.xdc.org"
       "https://rpc.ankr.com/xdc"
     ],
   },
