6f69473f16552b87c893d1a74638bc4d9c4a7a05
realdealshaman <realdealshaman11@gmail.com>
2023-10-30 21:31:25 +0000

    add testnet rpc

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -2769,7 +2769,8 @@ export const extraRpcs = {
         trackingDetails: privacyStatement.onfinality,
       },
       "https://evmos-json-rpc.0base.dev",
-      "https://json-rpc.evmos.tcnetwork.io"
+      "https://json-rpc.evmos.tcnetwork.io",
+      "https://evmos-tjson.antrixy.org/"
     ],
   },
   836542336838601: {
