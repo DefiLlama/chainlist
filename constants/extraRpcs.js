ef0192f0cd29ae7a998322f3a260ce1c00c87562
0xx92 <133748557+0xx92@users.noreply.github.com>
2023-10-30 11:37:44 +0800

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -3646,6 +3646,16 @@ export const extraRpcs = {
       "https://mainnet-rpc.curvescan.io",
     ],
   },
+
+  167007: {
+    rpcs: [
+      {
+        url: "https://taiko-jolnir.blockpi.network/v1/rpc/public",
+        tracking: "limited",
+        trackingDetails: privacyStatement.blockpi,
+      }
+    ]
+  }
   2323: {
     rpcs: [
       {
