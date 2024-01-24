5e54b6ac067373865a7dbfc99e9b3742d0da2367
Scryotic <mrswagery@gmail.com>
2024-01-24 10:23:18 +0700

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -2149,7 +2149,6 @@ export const extraRpcs = {
       {
         url: "https://holesky-rpc.nocturnode.tech",
         tracking: "none",
-        trackingDetails: privacyStatement.nocturnode,
       },
     ],
   },
