87258545312d013be46d3334c1b4f8741d46a588
Scryotic <mrswagery@gmail.com>
2024-01-27 05:04:40 +0700

    Update extraRpcs.js (adding Privacy policy of Nocturnode.tech) (#913)
    * Update extraRpcs.js
    
    * Update extraRpcs.js
    
    add privacy statement
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -140,6 +140,8 @@ blockswap: "Blockswap RPC does not track any kind of user information at the bui
     "We may collect information about how you interact with our Service. This may include information about your operating system, IP address, and browser type : https://nodeconnect.org/privacy.txt",
   decubate:
     "With the exception of data that will be public on chain, all the other metadata should remain private to users and other parties should not be able to access or collect it. Decubate doesn't store any data related to the user using the RPC. https://docs.decubate.com/rpc-privacy/",
+  nocturnDao:
+    "As a fundamental practice, we do not collect, store, or process any personal information from our users. This non-collection policy ensures absolute data security and privacy for our users.https://nocturnode.tech/privacy"
   };
 
 export const extraRpcs = {
@@ -2146,7 +2148,11 @@ export const extraRpcs = {
         tracking: "limited",
         trackingDetails: privacyStatement.blockpi,
       },
-     	"https://holesky-rpc.nocturnode.tech",
+      {
+	url: "https://holesky-rpc.nocturnode.tech",
+	tracking: "none",
+        trackingDetails: privacyStatement.NocturnDao,
+      },
     ],
   },
   22: {
