7f1b9aabe5eb2e41972281fddb953186e901b8ff
0xx92 <133748557+0xx92@users.noreply.github.com>
2024-02-04 09:55:18 +0800

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -2065,11 +2065,6 @@ export const extraRpcs = {
   88: {
     rpcs: ["https://rpc.tomochain.com",
       {
-        url: "https://tomo.blockpi.network/v1/rpc/public",
-        tracking: "limited",
-        trackingDetails: privacyStatement.blockpi,
-      },
-      {
         url: "https://viction.blockpi.network/v1/rpc/public",
         tracking: "limited",
         trackingDetails: privacyStatement.blockpi,
@@ -3436,11 +3431,6 @@ export const extraRpcs = {
         url: "https://scroll-alphanet.public.blastapi.io",
         tracking: "limited",
         trackingDetails: privacyStatement.blastapi,
-      },
-      {
-        url: "https://scroll-testnet.blockpi.network/v1/rpc/public",
-        tracking: "limited",
-        trackingDetails: privacyStatement.blockpi,
       },
       {
         url: "https://scroll-alphanet.public.blastapi.io",
