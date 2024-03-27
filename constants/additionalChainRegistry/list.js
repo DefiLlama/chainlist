1ae8c81c22c7c3ebf1d5805f05e084d47ccf0708
0xngmi <0xngmi@protonmail.com>
2024-03-27 17:16:42 +0000

    overwrite chains

---
+++ constants/additionalChainRegistry/list.js
@@ -0,0 +1,7 @@
+export const overwrittenChains = [
+    require("./chainid-3109.json"),
+    require("./chainid-10001.json"),
+    require("./chainid-88888.json"),
+    require("./chainid-5151706.json"),
+    require("./chainid-1380012617.json"),    
+]
