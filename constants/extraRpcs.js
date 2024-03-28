daed8206b04410dead12a72b068f9f58d7fca07f
blockchaingirl1407 <142514003+blockchaingirl1407@users.noreply.github.com>
2024-03-28 14:20:53 +0100

    Addint Tatum RPC node - Klaytn

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1365,6 +1365,11 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.drpc,
       },
+      {
+        url: "https://api.tatum.io/v3/blockchain/node/klaytn-cypress",
+        tracking: "yes",
+        trackingDetails: privacyStatement.tatum,
+      },
     ],
   },
   1666600000: {
