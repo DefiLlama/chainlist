f7586d8668707ae3717cd0689ffa83adc2f651e9
Ersan YAKIT <ersanyakit@yahoo.com.tr>
2024-04-03 00:14:52 +0300

    chiliz chain integration (#961)
    chiliz chain integration

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -3974,6 +3974,12 @@ export const extraRpcs = {
       "https://kekchain.interchained.org",
     ],
   },
+  88888: {
+    rpcs: ["https://rpc.chiliz.com"],
+  },
+  88882:{
+    rpcs:["https://spicy-rpc.chiliz.com"],
+  },
   420666: {
     rpcs: ["https://testnet.kekchain.com"],
   },
@@ -3994,7 +4000,7 @@ export const extraRpcs = {
   },
   943: {
     rpcs: [
-	     {
+	    {
         url: "https://pulsetest-s.projectpi.xyz",
         tracking: "none",
         trackingDetails: privacyStatement.projectpi,
