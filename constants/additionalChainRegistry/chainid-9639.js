134491d193c08ae69fec3e8c8f28749efc8a0a51
TikeshFlint <tikesh@flint-labs.com>
2024-04-05 11:25:59 +0530

    kartel chain addition chainlist

---
+++ constants/additionalChainRegistry/chainid-9639.js
@@ -0,0 +1,23 @@
+export const data = {
+    "name": "Kartel",
+    "chain": "kartel",
+    "icon": "ETH",
+    "rpc": [],
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "ETH",
+      "symbol": "ETH",
+      "decimals": 18
+    },
+    "infoURL": "",
+    "shortName": "kartel",
+    "chainId": 9639,
+    "networkId": 9639,
+    "explorers": [
+      {
+        "name": "",
+        "url": ""
+      }
+    ]
+  }
