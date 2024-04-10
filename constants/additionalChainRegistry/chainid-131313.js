c12ffb65caceeb660602c0b02901a651d42a5480
dreamdev2427 <protodev531@gmail.com>
2024-04-10 11:44:16 -0400

    added Odyssey chain information

---
+++ constants/additionalChainRegistry/chainid-131313.js
@@ -0,0 +1,17 @@
+export const data = {
+  name: "Odyssey Chain (Testnet)",
+  chain: "DIONE",
+  rpc: ["https://testnode.dioneprotocol.com/ext/bc/D/rpc"],
+  faucets: ["https://faucet.dioneprotocol.com/"],
+  nativeCurrency: {
+    name: "DIONE",
+    symbol: "DIONE",
+    decimals: 18,
+  },
+  infoURL: "https://www.dioneprotocol.com/",
+  shortName: "DIONE",
+  chainId: 131313,
+  networkId: 131313,
+  icon: "odyssey",
+  features: [{ name: "EIP155" }],
+};
