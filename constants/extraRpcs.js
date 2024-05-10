606d8059b6133b12a5c55333d0ebb1071df44133
coolestowl <97215481+coolestowl@users.noreply.github.com>
2024-05-10 23:29:35 +0800

    Update extraRpcs.js (#1088)
    remove rockx endpoint

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -164,8 +164,6 @@ blockswap: "Blockswap RPC does not track any kind of user information at the bui
     "Tatum Technology s.r.o.'s policy respects your privacy regarding any information we may collect from you across our website, https://tatum.io, and other sites we own and operate. For more info, check https://tatum.io/privacy-policy .",
   nodifi:
   "Nodifi AI privacy policy request no privacy intrusion. We do not track IP, wallets, or the websites connected to our nodes. For more info check https://nodifi.ai/privacy-policy",
-  rockx:
-    "We may automatically collect and store certain information about your interaction with our Applications including IP address, browser type, internet service provider, referring/exit pages, operating system, date/time stamps and related data. For more info check https://www.rockx.com/privacy-policy.",
   taikotools:
     "We don't gather: User IP addresses, wallets, sources of requests and request content. For more info check https://taiko.tools/privacy-policy",
   sigmacore:
@@ -1729,11 +1727,6 @@ export const extraRpcs = {
         url: "https://merlin.blockpi.network/v1/rpc/public",
         tracking: "limited",
         trackingDetails: privacyStatement.blockpi
-      },
-      {
-        url: "https://rockx-merlin.w3node.com/public-merlin/api",
-        tracking: "yes",
-        trackingDetails: privacyStatement.rockx
       }
     ]
   },
