5df95368a62dd9ee533d404846ee1a0027ec5584
yp-10221 <luleigreat@163.com>
2024-06-09 09:03:14 +0800

    Add nal testnet

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -5000,6 +5000,11 @@ export const extraRpcs = {
       "https://rpc.devnet.citrea.xyz",
     ]
   },
+  328527624: {
+    rpcs: [
+      "https://testnet-rpc.nal.network",
+    ]
+  },
 };
 const allExtraRpcs = mergeDeep(llamaNodesRpcs, extraRpcs);
 
