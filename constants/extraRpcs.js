c733a24ecebdf83105aa7150513b0f313853a7f6
realdealshaman <realdealshaman11@gmail.com>
2024-06-24 19:16:02 +0100

    update tracking details

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -50,8 +50,8 @@ blockswap: "Blockswap RPC does not track any kind of user information at the bui
     "We do not collect request data or request origin. We only temporarily record the request method names and IP addresses for 7 days to ensure our service functionality such as load balancing and DDoS protection. All the data is automatically deleted after 7 days and we do not store any user information for longer periods of time. https://blockpi.io/privacy-policy",
   payload:
     "Sent transactions are private: https://payload.de/docs. By default, no data is collected when using the RPC endpoint. If provided by the user, the public address for authentication is captured when using the RPC endpoint in order to prioritize requests under high load. This information is optional and solely provided at the user's discretion. https://payload.de/privacy/",
-  gitshock:
-    "We do not collect any personal data from our users. Our platform is built on blockchain technology, which ensures that all transactions are recorded on a public ledger that is accessible to all users. However, this information is anonymous and cannot be linked to any specific individual. https://docs.gitshock.com/users-guide/privacy-policy",
+  /*gitshock:
+    "We do not collect any personal data from our users. Our platform is built on blockchain technology, which ensures that all transactions are recorded on a public ledger that is accessible to all users. However, this information is anonymous and cannot be linked to any specific individual. https://docs.gitshock.com/users-guide/privacy-policy",*/ // website is down
   gashawk:
     "Sign-in with Ethereum on https://www.gashawk.io required prior to use. We may collect information that is publicly available in a blockchain when providing our services, such as: Public wallet identifier of the sender and recipient of a transaction, Unique identifier for a transaction, Date and time of a transaction, Transaction value, along with associated costs, Status of a transaction (such as whether the transaction is complete, in-progress, or resulted in an error), read the terms of service https://www.gashawk.io/#/terms and the privacy policy https://www.gashawk.io/#/privacy.",
   LiveplexOracleEVM:
@@ -1833,19 +1833,15 @@ export const extraRpcs = {
         trackingDetails: privacyStatement.omnia,
       },
    ],
-},
+  },
   1881: {
     rpcs: [
-      {
-        url: "https://rpc.cartenz.works",
-        tracking: "none",
-        trackingDetails: privacyStatement.gitshock,
-      }
+      "https://rpc.cartenz.works"
     ]
   },
-4200: {
-      rpcs: [
-	  {
+  4200: {
+    rpcs: [
+	    {
         url: "https://merlin.blockpi.network/v1/rpc/public",
         tracking: "limited",
         trackingDetails: privacyStatement.blockpi
