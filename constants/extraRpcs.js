4cf4597ae950ec59225363c6305902185d4df1ae
NuCash <39842364+nucash-mining@users.noreply.github.com>
2024-06-27 12:54:42 -0500

    Update extraRpcs.js (#1166)
    * Update extraRpcs.js
    
    added to lines 1837-1858 added the following chains and rpc nodes
    
    ```2330: {
        rpcs: [
          {
            url: "http://138.197.152.181:8145",
            tracking: "none",
            trackingDetails: privacyStatement.gitshock,
          },
          {
            url: "https://rpc0.altcoinchain.org/rpc",
            tracking: "none",
            trackingDetails: privacyStatement.gitshock,
          }
        ]
      },	
     1773: {
        rpcs: [
          {
            url: "http://138.197.152.181:8245",
            tracking: "none",
            trackingDetails: privacyStatement.gitshock,
          }
        ]
      },```
    
    * Update extraRpcs.js
    
    remove incorrect info
    
    ---------
    
    Co-authored-by: Real Shaman <85087525+realdealshaman@users.noreply.github.com>

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1849,6 +1849,17 @@ export const extraRpcs = {
       },
    ],
   },
+  2330: {
+    rpcs: [
+      "http://138.197.152.181:8145",
+      "https://rpc0.altcoinchain.org/rpc"
+    ]
+  },	
+ 1773: {
+    rpcs: [
+     "http://138.197.152.181:8245"
+    ]
+  },
   1881: {
     rpcs: [
       "https://rpc.cartenz.works"
