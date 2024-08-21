dc1654ef50843decfca56f2411990536e1058766
Edwin <lark930@gmail.com>
2024-08-21 09:35:08 +0800

    Add Bifrost Polkadot Mainnet (#1229)

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -5543,6 +5543,9 @@ export const extraRpcs = {
       },
     ],
   },
+  996: {
+    rpcs: ["https://hk.p.bifrost-rpc.liebi.com"],
+  },
 };
 const allExtraRpcs = mergeDeep(llamaNodesRpcs, extraRpcs);
 
