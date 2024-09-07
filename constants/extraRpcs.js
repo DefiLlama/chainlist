b962ee73b4f424874a80c878a5f8870f33ccb2ee
Daniel (OnFinality) <92831040+Danielvul@users.noreply.github.com>
2024-09-07 12:27:09 +1200

    Update extraRpcs.js (#1248)
    Added OnFinality Mantle endpoint

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -3608,6 +3608,11 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.onerpc,
       },
+      {
+        url: "https://mantle.api.onfinality.io/public",
+        tracking: "limited",
+        trackingDetails: privacyStatement.onfinality,
+      },
     ],
   },
   5197: {
