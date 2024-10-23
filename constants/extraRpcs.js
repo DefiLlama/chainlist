8b0b9c640c7d75fa3880ab3f7b72a347cc39d960
realdealshaman <realdealshaman11@gmail.com>
2024-10-23 06:08:47 +0100

    remove duplicate entry

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1306,11 +1306,6 @@ export const extraRpcs = {
       }
     ],
   },
-  33139: {
-    rpcs: [
-      "https://rpc.apechain.com/http",
-    ]
-  },
   42161: {
     rpcs: [
       "https://arb1.arbitrum.io/rpc",
