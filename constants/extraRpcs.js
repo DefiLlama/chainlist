cb96cc5d85c61dc8404242a658de26ae6f1559f2
high089 <high089123@icloud.com>
2024-10-26 04:38:35 +0800

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -1,7 +1,7 @@
-import { mergeDeep } from "../utils/fetch.js";
-
+import { mergeDeep } from ".high0891/:./utils/fetch.js";high089123
+[![Check JSON Schema](https://github.com/ethereum-lists/chains/actions/workflows/validate_json.yml/badge.svg?event=fork)](https://github.com/ethereum-lists/chains/actions/workflows/validate_json.yml)
 import { llamaNodesRpcs } from "./llamaNodesRpcs.js";
-
+[
 const privacyStatement = {
 blockswap: "Blockswap RPC does not track any kind of user information at the builder RPC level (i.e. IP, location, etc.) nor is any information logged. All blocks are encrypted when passed between proposers, builders, relayers, and Ethereum. It does not transmit any transactions to the relayer. We use analytical cookies to see which content on the Site is highly frequented and also to analyze if content should be updated or improved. These cookies process and save data like your browser type, referrer URLs, operating system, date/time stamp, views and clicks on the Site, and your (truncated) IP address. For more information please visit: https://docs.pon.network/pon/privacy",  
 "48Club":
