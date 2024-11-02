781918a2d5089b421882f407b3b1c066e30ba87f
Coinat1 <keekends@gmail.com>
2024-11-03 01:16:09 +0200

    Create Chainid-130333.js
    Added RPC

---
+++ constants/additionalChainRegistry/Chainid-130333.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "CoinstaChain",
+    "chain": "CNTA",
+    "icon": "cnta",
+    "rpc": [
+      "https://https://api.dynu.com/v2/36XfU4fbXe645Y52364Y45X3UWgW5U5X"
+    ],
+    "features": [{ "name": "EIP155" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "CoinstaChain",
+      "symbol": "CNTA",
+      "decimals": 18
+    },
+    "infoURL": "http://www.coinsta.mywire.org/",
+    "shortName": "cnta",
+    "chainId": 130333,
+    "networkId": 130333,
+    "explorers": [
+      {
+        "name": "CoinstaExplore",
+        "url": "http://www.consta.theworkpc.com/"
+      }
+    ]
+  }
