fc40e51f33195f8405c2da7a6dc8e24032e14773
0xAshcry <77532412+0xAshcry@users.noreply.github.com>
2024-11-26 08:40:56 +0700

    Update extraRpcs.js (#1401)
    https://x.com/0xAshcry
    https://testnet.storyscan.app/validators/storyvaloper1u2vlklgdfjnah2ll7gghp2cvw4j7x7asynxxdn

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -5852,6 +5852,7 @@ export const extraRpcs = {
       "https://evm-rpc-story.josephtran.xyz",
       "https://story.evm.t.stavr.tech",
       "https://story-testnet-jsonrpc.blockhub.id",
+      "https://story-testnet-jsonrpc.daaps-j4ran.cloud",
       {
         url: "https://story-testnet-evm.itrocket.net",
         tracking: "none",
