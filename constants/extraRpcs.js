bc7c218d23522dee87c1958fe07b1a9e2eb6e469
silentnoname <44841346+silentnoname@users.noreply.github.com>
2025-02-13 04:25:12 +0800

    Add silent validator RPC for Story mainnet (#1545)

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -7129,7 +7129,8 @@ export const extraRpcs = {
       "https://evm-rpc-story.j-node.net",
       "https://lightnode-json-rpc-mainnet-story.grandvalleys.com",
       "https://story-evm-rpc.krews.xyz",
-      "https://story-mainnet-jsonrpc.blockhub.id"
+      "https://story-mainnet-jsonrpc.blockhub.id",
+      "https://evm-rpc.story.silentvalidator.com"
     ],
   },
   3030: {
