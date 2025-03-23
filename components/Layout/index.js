dc0d65fd87f6c391fb24cdff58a66d29b82db7ba
0xngmi <0xngmi@protonmail.com>
2025-03-23 09:51:25 +0000

    add api link

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -121,6 +121,18 @@ export default function Layout({ children, lang, chainName, setChainName }) {
             </svg>
             <span className="text-base font-medium">{t("toggle-theme")}</span>
           </a>
+
+          <a
+            className="flex items-center gap-2 mx-auto lg:ml-0"
+            href="/rpcs.json"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6" fill="none" stroke="#2F80ED" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline>
+            </svg>
+            <span className="text-base font-medium">API</span>
+          </a>
         </div>
       </div>
       <div className="dark:bg-[#181818] bg-[#f3f3f3] p-5 relative flex flex-col gap-5">
