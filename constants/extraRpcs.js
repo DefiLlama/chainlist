52bac20b4bba34f9c4627320c0441a6a3ecacfc2
Melandri <56926590+sSelmann@users.noreply.github.com>
2025-05-03 04:08:33 +0300

    Update extraRpcs.js

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -7959,6 +7959,7 @@ export const extraRpcs = {
     rpcs: [
       "https://aeneid.storyrpc.io/",
       "https://evm-aeneid-story.j-node.net",
+      "https://aeneid-evm-rpc.krews.xyz",
       {
         url: "https://lightnode-json-rpc-story.grandvalleys.com",
         tracking: "none",
