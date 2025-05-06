f1354fb12f51f7040f68a62fe3906a79400cbd65
Coha05 <119700026+Coha05@users.noreply.github.com>
2025-05-06 08:42:03 +0700

    Update story aeneid rpc (#1652)

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -7970,6 +7970,7 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.ankr,
       },
+      "https://story-aeneid-rpc.spidernode.net",
     ],
   },
   224433: {
