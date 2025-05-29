52c87a67758475a2383bf741386bf3ee7b895871
ICB Network <153494910+ICBNetwork@users.noreply.github.com>
2025-05-29 18:48:07 +0400

    ICBX Network Mainnet  New RPC's | Update extraRpcs
    Network Name :- ICB Network 
    Chain ID :- 73115(0x11d9b)
    Currency : ICBX

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -8253,6 +8253,26 @@ export const extraRpcs = {
       },
     ],
   },
+
+  73115: {
+    rpcs: [
+      {
+        url: "https://rpc1-mainnet.icbnetwork.info",
+        tracking: "none",
+        trackingDetails: privacyStatement.drpc,
+      },
+      {
+        url: "https://rpc2-mainnet.icbnetwork.info",
+        tracking: "none",
+        trackingDetails: privacyStatement.drpc,
+      },
+      {
+        url: "https://main1.rpc-icb-network.io/",
+        tracking: "none",
+        trackingDetails: privacyStatement.drpc,
+      },
+    ],
+  },
 };
 
 const allExtraRpcs = mergeDeep(llamaNodesRpcs, extraRpcs);
