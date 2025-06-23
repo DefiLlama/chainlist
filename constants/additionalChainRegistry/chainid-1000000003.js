e57432550568992a8ce9eee66684a0c363fca407
jackkru69 <www.jackkru69@mail.ru>
2025-06-23 18:45:41 +0300

    Add Deinfra Mainnet and Deinfra Dev 3

---
+++ constants/additionalChainRegistry/chainid-1000000003.js
@@ -0,0 +1,22 @@
+export const data = {
+  "name": "DeInfra Devnet3",
+  "chain": "DeInfraDev3",
+  "rpc": ["https://c3n1.thepower.io:1446/jsonrpc"],
+  "faucets": ["https://faucet.thepower.io/"],
+  "nativeCurrency": {
+    "name": "dev3SK",
+    "symbol": "dSK",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://deinfra.net",
+  "shortName": "deinfra-dev3",
+  "chainId": 1000000003,
+  "networkId": 1000000003,
+  "icon": "deinfra",
+  "explorers": [{
+    "name": "Deinfra Devnet Chain 3 Network Explorer",
+    "url": "https://bs.thepower.io",
+    "standard": "EIP3091"
+  }]
+}
