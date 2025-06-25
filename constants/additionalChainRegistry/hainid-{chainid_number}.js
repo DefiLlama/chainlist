2d924002fdd5798e8b0502eda00f185b345e2dcc
sheela <delhi0047@gmail.com>
2025-06-25 22:39:08 +0530

    Create hainid-{chainid_number}.js

---
+++ constants/additionalChainRegistry/hainid-{chainid_number}.js
@@ -0,0 +1,25 @@
+{
+  "name": "Ethereum Mainnet",
+  "chain": "ETH",
+  "rpc": [
+    "https://eth.llamarpc.com",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://ethereum.org",
+  "shortName": "eth",
+  "chainId": 1,
+  "networkId": 1,
+  "icon": "ethereum",
+  "explorers": [{
+    "name": "etherscan",
+    "url": "https://etherscan.io",
+    "icon": "etherscan",
+    "standard": "EIP3091"
+  }]
+}
