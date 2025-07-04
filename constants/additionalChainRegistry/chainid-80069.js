1f67dd745fbcf3cdf7784e80b7bec6e2e1e93217
Camembear <camembear@berachain.com>
2025-07-04 09:52:43 -0400

    add Berachain Bepolia details

---
+++ constants/additionalChainRegistry/chainid-80069.js
@@ -0,0 +1,44 @@
+{
+        "name": "Berachain Bepolia",
+        "chain": "TBERA",
+        "rpc": [
+                "https://bepolia.rpc.berachain.com"
+        ],
+        "faucets": ["https://bepolia.faucet.berachain.com/"],
+        "nativeCurrency": {
+                "name": "BERA",
+                "symbol": "BERA",
+                "decimals": 18
+        },
+        "features": [
+                { "name": "EIP155" },
+                { "name": "EIP1559" },
+                { "name": "EIP2930" },
+                { "name": "EIP4844" },
+                { "name": "EIP1153" },
+                { "name": "EIP4788" },
+                { "name": "EIP5656" },
+                { "name": "EIP6780" },
+                { "name": "EIP7702" },
+                { "name": "EIP2537" },
+                { "name": "EIP2935" },
+                { "name": "EIP6110" },
+                { "name": "EIP7002" }
+        ],
+        "infoURL": "https://berachain.com",
+        "shortName": "bepolia",
+        "chainId": 80069,
+        "networkId": 80069,
+        "icon": "berachain",
+        "explorers": [{
+                "name": "berascan-bepolia",
+                "url": "https://testnet.berascan.com",
+                "icon": "etherscan",
+                "standard": "EIP3091"
+        },{
+                "name": "beratrail-bepolia",
+                "url": "https://bepolia.beratrail.io",
+                "icon": "routescan",
+                "standard": "EIP3091"
+        }]
+}
