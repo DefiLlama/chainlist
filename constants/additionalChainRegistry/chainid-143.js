bf23d649644ce6e8301b2b8ea9a281a214127dbd
Đorđe Mijović <djordje.mijovic@gmail.com>
2025-07-17 13:25:41 +0200

    Add monad mainnet to chainlist.

---
+++ constants/additionalChainRegistry/chainid-143.js
@@ -0,0 +1,18 @@
+{
+  "name": "Monad Mainnet",
+  "chain": "MON",
+  "rpc": [],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "MON Token",
+    "symbol": "MON",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://monad.xyz",
+  "shortName": "mon",
+  "chainId": 143,
+  "networkId": 143,
+  "icon": "monad",
+  "explorers": []
+}
