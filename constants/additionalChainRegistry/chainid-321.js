e870d3a6daaf61e2b8dc59fc6016d607ce20b670
Shahrad Elahi <shahrad@litehex.com>
2025-07-28 02:40:14 +0100

    Add KCC Mainnet

---
+++ constants/additionalChainRegistry/chainid-321.js
@@ -0,0 +1,37 @@
+export const data = {
+  "name": "KCC Mainnet",
+  "chain": "KCC",
+  "rpc": [
+    "https://rpc-mainnet.kcc.network",
+    "https://kcc-rpc.com",
+    "wss://rpc-ws-mainnet.kcc.network",
+    "wss://ws.kcc-rpc.com",
+  ],
+  "faucets": [
+    "https://faucet.kcc.io/",
+  ],
+  "nativeCurrency": {
+    "name": "KuCoin Token",
+    "symbol": "KCS",
+    "decimals": 18,
+  },
+  "infoURL": "https://kcc.io",
+  "shortName": "kcc",
+  "chainId": 321,
+  "networkId": 321,
+  "icon": "kcc",
+  "explorers": [
+    {
+      "name": "KCC Explorer",
+      "url": "https://explorer.kcc.io/en",
+      "icon": "kcc",
+      "standard": "EIP3091",
+    },
+    {
+      "name": "KCC Scan",
+      "url": "https://scan.kcc.io",
+      "icon": "kcc",
+      "standard": "EIP3091",
+    },
+  ],
+};
