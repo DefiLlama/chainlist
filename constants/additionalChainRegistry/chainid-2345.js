e77bb961660614620477a22fdc3b7e9a498e10db
Aiden <aiden.a@metis.io>
2025-08-08 16:21:46 +0800

    chore: add additional chain registry with goat network 2345

---
+++ constants/additionalChainRegistry/chainid-2345.js
@@ -0,0 +1,26 @@
+export const data =
+{
+  "name": "GOAT Network",
+  "chain": "GOAT",
+  "rpc": [
+    "https://rpc.goat.network"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Bitcoin",
+    "symbol": "BTC",
+    "decimals": 18
+  },
+
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://www.goat.network",
+  "shortName": "goat",
+  "chainId": 2345,
+  "networkId": 2345,
+  "icon": "goat",
+  "explorers": [{
+    "name": "GOAT Explorer",
+    "url": "https://explorer.goat.network",
+    "standard": "EIP3091"
+  }]
+}
