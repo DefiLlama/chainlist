6f31ce5fda8463fec27b98a52503464e43855a48
JohnPochta <johnpochta1@gmail.com>
2025-08-11 17:22:22 +0200

    feat(stable-2201): added stable chain, chainId 2201

---
+++ constants/additionalChainRegistry/chain-2201.js
@@ -0,0 +1,22 @@
+export const data = {
+  name: 'Stable Testnet',
+  chain: 'stabletestnet_2201-1',
+  rpc: ['https://stable-jsonrpc.testnet.chain0.dev'],
+  icon: 'stable',
+  faucets: ['https://demo.testnet.chain0.dev/faucet'],
+  nativeCurrency: {
+    name: 'USDT',
+    symbol: 'USDT',
+    decimals: 18,
+  },
+  features: [{ name: 'EIP155' }, { name: 'EIP1559' }],
+  infoURL: 'https://docs.partners.stable.xyz/testnet/testnet-information',
+  shortName: 'STABLE',
+  chainId: 2201,
+  networkId: 2201,
+  explorers: [{
+    name: 'Stable Explorer',
+    url: 'https://stable-explorer.testnet.chain0.dev',
+    standard: 'EIP3091'
+  }],
+};
