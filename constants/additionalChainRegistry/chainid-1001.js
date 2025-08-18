6bbb60400bf3fef365f4802dedf4747c3d88e498
AMI <70102538+Airpote@users.noreply.github.com>
2025-08-18 15:58:32 +0200

    Adding chain

---
+++ constants/additionalChainRegistry/chainid-1001.js
@@ -0,0 +1,42 @@
+{
+  "name": "Martian Chain",
+  "chain": "EROL",
+  "rpc": [
+    "https://martian-rpc1.martianchain.com",
+	"https://martian-rpc2.martianchain.com",
+	"https://martian-rpc3.martianchain.com",
+	"https://martian-rpc4.martianchain.com",
+	"https://martian-rpc5.martianchain.com"
+  ],
+  "faucets": [
+  "app.ami.finance"
+  ],
+  "nativeCurrency": {
+    "name": "Erol Musk",
+    "symbol": "EROL",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "martianchain.com",
+  "shortName": "Erol",
+  "chainId": 1001,
+  "networkId": 1001,
+  "icon": "https://martianchain.com/img/martian.png",
+  "explorers": [{
+    "name": "routescan",
+    "url": "https://devnet.routescan.io/?rpc=https://rpc1.martianchain.com",
+    "icon": "https://cdn.routescan.io/cdn/svg/routescan-new.svg",
+    "standard": "EIP3091"
+  },
+  {
+    "name": "subnets avax",
+    "url": "https://subnets.avax.network/subnets/28aQXYENwytzxEwyYMZDtGjpUmP67eWkyoHdGGyid6gEACeg9x",
+    "icon": "avax",
+    "standard": "EIP3091"
+  },
+  {
+    "name": "ErolExplorer",
+    "url": "https://explorer.martianchain.com",
+    "standard": "EIP3091"
+  }]
+}
