f386e2b7a2ebd8c6446ab9b240efbd5e5631f469
AMI <70102538+Airpote@users.noreply.github.com>
2025-08-18 15:55:40 +0200

    Adding Ami Chain

---
+++ constants/additionalChainRegistry/chainid-588785742.js
@@ -0,0 +1,35 @@
+{
+  "name": "Ami Chain",
+  "chain": "Ami",
+  "rpc": [
+    "https://rpc.amichain.org",
+	"https://rpc-2.amichain.org",
+	"https://rpc-3.amichain.org",
+	"https://rpc-4.amichain.org",
+	"https://rpc-5.amichain.org"
+  ],
+  "faucets": [
+  "app.ami.finance"
+  ],
+  "nativeCurrency": {
+    "name": "Avax Meme Index",
+    "symbol": "AMI",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "amichain.org",
+  "shortName": "Ami",
+  "chainId": 588785742,
+  "networkId": 588785742,
+  "icon": "https://www.amichain.org/images/ami-logo.png",
+  "explorers": [{
+    "name": "routescan",
+    "url": "https://devnet.routescan.io/?rpc=https://rpc.amichain.org,
+    "icon": "https://cdn.routescan.io/cdn/svg/routescan-new.svg",
+    "standard": "EIP3091"
+  },{
+    "name": "Ami Explorer",
+    "url": "https://explorer.ami.finance",
+    "standard": "EIP3091"
+  }]
+}
