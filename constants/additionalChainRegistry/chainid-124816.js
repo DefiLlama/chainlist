72f2eddbe455618105e607dce63e3502901bfea2
Taeguk Kwon <xornrbboy@gmail.com>
2025-08-21 21:41:26 +0700

    Add mitosis chain to additional registry

---
+++ constants/additionalChainRegistry/chainid-124816.js
@@ -0,0 +1,18 @@
+export const data = {
+  name: "Mitosis",
+  chain: "MITO",
+  rpc: ["https://rpc.mitosis.org"],
+  faucets: [],
+  nativeCurrency: {
+    name: "Mitosis",
+    symbol: "MITO",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://mitosis.org",
+  shortName: "mitosis",
+  chainId: 124816,
+  networkId: 124816,
+  icon: "https://storage.googleapis.com/mitosis-statics/logos/mitosis_logo_symbol_basic.png",
+  explorers: [],
+};
