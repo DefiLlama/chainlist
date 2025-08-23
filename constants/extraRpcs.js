a292e81d5d462fc7c0fc2f70447672dd2a5a710b
blockchaingirl1407 <142514003+blockchaingirl1407@users.noreply.github.com>
2025-08-23 20:23:16 +0200

    Please Add Tatum Flow Mainnet RPC endpoint (#1940)
    Please Add Tatum Flow Mainnet RPC endpoint

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -6327,6 +6327,11 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.Histori,
       },
+      {
+        url: "https://flow-mainnet.gateway.tatum.io/",
+        tracking: "yes",
+        trackingDetails: privacyStatement.tatum,
+      },
     ],
   },
   1707: {
