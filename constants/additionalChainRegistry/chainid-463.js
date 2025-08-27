a2fed1ff8d701a4a83ca392f60578aa61cc53b74
Emrah <113093777+emrahsky@users.noreply.github.com>
2025-08-27 12:35:43 +0300

    areon to areum

---
+++ constants/additionalChainRegistry/chainid-463.js
@@ -0,0 +1,34 @@
+export const data = {
+    "name": "Areum Mainnet",
+    "chain": "AREUM",
+    "rpc": [
+      "https://mainnet-rpc.areon.network",
+      "https://mainnet-rpc2.areon.network",
+      "https://mainnet-rpc3.areon.network",
+      "https://mainnet-rpc4.areon.network",
+      "https://mainnet-rpc5.areon.network",
+      "https://mainnet-rpc.areum.network",
+      "https://mainnet-rpc2.areum.network",
+      "https://mainnet-rpc3.areum.network",
+      "https://mainnet-rpc4.areum.network",
+      "https://mainnet-rpc5.areum.network",
+    ],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Areum",
+      "symbol": "AREA",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }],
+    "infoURL": "https://areum.network",
+    "shortName": "areum",
+    "chainId": 463,
+    "networkId": 463,
+    "icon": "areum",
+    "explorers": [{
+      "name": "Areum Explorer",
+      "url": "https://explorer.areum.network",
+      "icon": "areum",
+      "standard": "EIP3091"
+    }]
+  }
