a95032696e2b2e73bd298fae26b75aa4e3c7bd8c
Mike Christopher <mchristopher@users.noreply.github.com>
2025-08-29 09:52:03 -0700

    feat: add Fuel Mainnet

---
+++ constants/additionalChainRegistry/chainid-9889.js
@@ -0,0 +1,16 @@
+export const data = {
+    "name": "Fuel Mainnet",
+    "chain": "FUEL",
+    "rpc": [],
+    "features": [],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Ether",
+      "symbol": "ETH",
+      "decimals": 18
+    },
+    "infoURL": "https://docs.fuel.network/",
+    "shortName": "fuel",
+    "chainId": 9889,
+    "networkId": 9889,
+};
