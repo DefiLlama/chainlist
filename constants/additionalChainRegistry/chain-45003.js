ca99c5307ab2b2ac6682bdcdc5c709cdbacbb1b7
Sourav De <72644559+SRV-YouSoRandom@users.noreply.github.com>
2025-09-06 19:28:24 +0530

    Create chain-45003.js
    Adding Juneo Supernet to ChainList dir

---
+++ constants/additionalChainRegistry/chain-45003.js
@@ -0,0 +1,25 @@
+{
+  "name": "Juneo Mainnet",
+  "chain": "JUNE",
+  "rpc": [
+    "https://rpc.juneo-mainnet.network/ext/bc/JUNE/rpc",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "June",
+    "symbol": "JUNE",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": ["https://juneosupernet.com","https://juneo.com"],
+  "shortName": "juneo",
+  "chainId": 45003,
+  "networkId": 2,
+  "icon": "",
+  "explorers": [{
+    "name": "juneoscan",
+    "url": "https://juneoscan.io/chain/2",
+    "icon": "juneoscan",
+    "standard": "EIP3091"
+  }]
+}
