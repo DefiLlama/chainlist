6ab7b7162e6d1a345663153e599042802e612257
Nikita Zasimuk <nzasimuk@gmail.com>
2025-09-09 11:18:42 +0300

    feat: book chainId for Athena Terminal Mainnet

---
+++ constants/additionalChainRegistry/chainid-1380270412.js
@@ -0,0 +1,16 @@
+export const data = {
+  "name": "Athena Terminal",
+  "title": "Athena Terminal",
+  "chain": "Athena Terminal",
+  "rpc": [],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "USDe",
+    "symbol": "USDe",
+    "decimals": 18
+  },
+  "shortName": "athena-terminal",
+  "chainId": 1380270412,
+  "networkId": 1380270412,
+  "explorers": []
+}
