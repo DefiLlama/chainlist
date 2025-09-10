12975df604a761f5b9f74c28b8b22daf6f030d3e
Till Ziegler <tz@schoeneweide.tk>
2025-09-10 11:38:44 +0200

    Create chainid-420042.js

---
+++ constants/additionalChainRegistry/chainid-420042.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Vector Smart Chain",
+  "chain": "VECTOR",
+  "rpc": [
+    "https://rpc.vscblockchain.org"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Vector Smart Gas",
+    "symbol": "VSG",
+    "decimals": 18
+  },
+  "infoURL": "https://vsgofficial.com",
+  "shortName": "vector",
+  "chainId": 420042,
+  "networkId": 420042,
+  "icon": "vector",
+  "explorers": [{
+    "name": "blockscout",
+    "url": "https://explorer.vscblockchain.org",
+    "standard": "EIP3091"
+  }]
+}
