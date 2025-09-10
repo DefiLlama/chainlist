6d89b0bfe40449c714a0fea6e076b627686c5bd5
Define101 <93603962+Define101@users.noreply.github.com>
2025-09-10 09:58:59 +0100

    1991 (#2007)

---
+++ constants/additionalChainRegistry/chainid-510.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "Syndicate Mainnet",
+  "chain": "Syndicate",
+  "shortName": "syndicate",
+  "infoURL": "https://syndicate.io",
+  "icon": "syndicate",
+  "chainId": 510,
+  "networkId": 510,
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "Syndicate",
+    "symbol": "SYND",
+    "decimals": 18
+  },
+  "rpc": ["https://rpc.syndicate.io"],
+  "faucets": [],
+  "explorers": [
+    {
+      "name": "Syndicate Explorer",
+      "url": "https://explorer.syndicate.io",
+      'logo': 'blockscout',
+      "standard": "EIP3091"
+    }
+  ]
+}
---
+++ constants/additionalChainRegistry/chainid-51014.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "Risa Testnet",
+  "chain": "Risa Testnet",
+  "shortName": "risa",
+  "infoURL": "https://syndicate.io",
+  "icon": "syndicate",
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "chainId": 51014,
+  "networkId": 51014,
+  "nativeCurrency": {
+    "name": "Testnet Syndicate",
+    "symbol": "SYND",
+    "decimals": 18
+  },
+  "rpc": ["https://rpc.testnet.syndicate.io"],
+  "faucets": [],
+  "explorers": [
+    {
+      "name": "Risa Testnet Explorer",
+      "url": "https://explorer.testnet.syndicate.io",
+      'logo': 'blockscout',
+      "standard": "EIP3091"
+    }
+  ]
+}
