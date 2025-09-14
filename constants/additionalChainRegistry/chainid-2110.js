591ea81b7a1c2904f07358a2671721a9e50bd63a
andrepatta <9854773+andrepatta@users.noreply.github.com>
2025-09-14 20:36:29 -0300

    Add Parallax mainnet & testnet

---
+++ constants/additionalChainRegistry/chainid-2110.js
@@ -0,0 +1,15 @@
+export const data = {
+  name: "Parallax",
+  chain: "PARALLAX",
+  features: [{ name: "EIP155" }],
+  faucets: [],
+  nativeCurrency: {
+    name: "Parallax",
+    symbol: "PRLX",
+    decimals: 18,
+  },
+  infoURL: "https://github.com/microstack-tech/parallax-whitepaper",
+  shortName: "parallax",
+  chainId: 2110,
+  networkId: 2110,
+};
---
+++ constants/additionalChainRegistry/chainid-2111.js
@@ -0,0 +1,15 @@
+export const data = {
+  name: "Parallax Testnet",
+  chain: "PARALLAX",
+  features: [{ name: "EIP155" }],
+  faucets: [],
+  nativeCurrency: {
+    name: "Parallax",
+    symbol: "tPRLX",
+    decimals: 18,
+  },
+  infoURL: "https://github.com/microstack-tech/parallax-whitepaper",
+  shortName: "parallax",
+  chainId: 2111,
+  networkId: 2111,
+};
