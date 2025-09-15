fec2210e6bcce0635016026b81395f03cfe43b17
Xinyu <164192609+Solovyov1796@users.noreply.github.com>
2025-09-15 18:15:28 +0800

    Add 0G Mainnet chainId: 16661 (#2014)
    * Add 0g chain mainnet chainId: 16661
    
    * update chain name
    
    ---------
    
    Co-authored-by: Solovyov1796 <xinyu@0g.ai>

---
+++ constants/additionalChainRegistry/chainid-16661.js
@@ -0,0 +1,29 @@
+export const data = {
+  "name": "0G Mainnet",
+  "chain": "0G",
+  "rpc": [
+    "https://evmrpc.0g.ai"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "0G",
+    "symbol": "0G",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://0g.ai",
+  "shortName": "0g",
+  "chainId": 16661,
+  "networkId": 16661,
+  "testnet": false,
+  "explorers": [
+    {
+      "name": "0G Chain Explorer",
+      "url": "https://chainscan.0g.ai",
+      "standard": "EIP3091"
+    }
+  ]
+}
