f003705e28202d520667cb74c2ed58851bf27f5c
efrazee@gmail.com <efrazee@gmail.com>
2025-09-19 13:32:53 -0400

    Create chainid-5464.js
    add config for SagaEVM chain

---
+++ constants/additionalChainRegistry/chainid-5464.js
@@ -0,0 +1,24 @@
+export const data = {
+    "name": "SagaEVM",
+    "chain": "sagaevm",
+    "rpc": [
+        "https://sagaevm.jsonrpc.sagarpc.io",
+    ],
+    "faucets": [],
+    "nativeCurrency": {
+        "name": "GAS",
+        "symbol": "GAS",
+        "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }],
+    "infoURL": "https://www.saga.xyz/sagaevm",
+    "shortName": "sagaevm",
+    "chainId": 5464,
+    "networkId": 5464,
+    "icon": "saga",
+    "explorers": [{
+        "name": "SagaEVM Explorer",
+        "url": "https://sagaevm.sagaexplorer.io",
+        "icon": "saga"
+    }]
+}
