8b67f38a0ef024648ac2395bf140b8517ca12726
Ethena Luxora <vegapunkweb3@gmail.com>
2025-09-21 00:59:39 +0700

    Create chainid-648529.js

---
+++ constants/additionalChainRegistry/chainid-648529.js
@@ -0,0 +1,24 @@
+export const data = {
+  "name": "Modulax Mainnet",
+  "chain": "MDX",
+  "rpc": [
+    "https://rpc.modulax.org",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Modulax",
+    "symbol": "MDX",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://modulax.org",
+  "shortName": "mdx",
+  "chainId": 648529,
+  "networkId": 648529,
+  "icon": "modulax",
+  "explorers": [{
+    "name": "modulax",
+    "url": "https://explorer.modulax.org",
+    "icon": "modulax",
+  }]
+}
