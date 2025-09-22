ab01796818cd5ec23622728a2a5242b96a9a740b
Arman Aurobindo <66505181+armanthepythonguy@users.noreply.github.com>
2025-09-22 17:22:26 +0530

    added-horizen-testnet (#2038)

---
+++ constants/additionalChainRegistry/chainid-845320009.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Horizen Testnet",
+  "chain": "ZEN",
+  "rpc": [
+    "https://horizen-rpc-testnet.appchain.base.org",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://www.horizen.io/",
+  "shortName": "zen",
+  "chainId": 845320009,
+  "networkId": 845320009,
+  "explorers": [{
+    "name": "blockscout",
+    "url": "https://horizen-explorer-testnet.appchain.base.org/",
+    "standard": "EIP3091"
+  }]
+}
