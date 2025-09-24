b870025030f7b1cb32d61ff183a1bfa877c8c5e7
mihailo-maksa <mihajlomaksa9+@gmail.com>
2025-09-24 19:38:03 +0200

    add Intuition mainnet (chain ID: 1155)

---
+++ constants/additionalChainRegistry/chainid-1155.js
@@ -0,0 +1,25 @@
+export const data = {
+  name: "Intuition Mainnet",
+  chain: "INTUITION",
+  rpc: ["https://rpc.intuition.systems"],
+  faucets: [],
+  nativeCurrency: {
+    name: "Intuition",
+    symbol: "TRUST",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://intuition.systems",
+  shortName: "intuition-mainnet",
+  chainId: 1155,
+  networkId: 1155,
+  icon: "intuition",
+  explorers: [
+    {
+      name: "IntuitionScan (Mainnet)",
+      url: "https://explorer.intuition.systems",
+      standard: "EIP3091",
+    },
+  ],
+  testnet: false,
+};
