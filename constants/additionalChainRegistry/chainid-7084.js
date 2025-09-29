69ec7720a6cfb1dabe544cbc3a4a161c912b00af
unknown <megha.shukla@zeeve.io>
2025-09-30 01:48:44 +0530

    Add Growfitter Custom Chain to Chainlist

---
+++ constants/additionalChainRegistry/chainid-7084.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "Growfitter Mainnet",
+  "chain": "Growfitter",
+  "icon": "https://f005.backblazeb2.com/file/tracehawk-prod/logo/GrowFitter/Light.png",
+  "rpc": [
+    "https://rpc-mainnet-growfitter-rl.cogitus.io/ext/bc/2PdUCtQocNDvbVWy8ch4PdaicTHA2h5keHLAAPcs9Pr8tYaUg3/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "GFIT",
+    "symbol": "GFIT",
+    "decimals": 18
+  },
+  "infoURL": "https://www.growfitter.com/",
+  "shortName": "Growfitter-mainnet",
+  "chainId": 7084,
+  "networkId": 7084,
+  "explorers": [
+    {
+      "name": "Growfitter Explorer",
+      "url": "https://explorer-growfitter-mainnet.cogitus.io",
+      "icon": "https://f005.backblazeb2.com/file/tracehawk-prod/logo/GrowFitter/Light.png",
+      "standard": "EIP3091"
+    }
+  ]
+}
