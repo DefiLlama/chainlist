fd498468e001fb2194b506303b62d8a9964842ef
Joonas Lehtimäki <joonas.lehtimaki@gmail.com>
2025-10-02 12:30:47 +0300

    Add Warden Protocol (#2066)

---
+++ constants/additionalChainRegistry/chainid-8765.js
@@ -0,0 +1,21 @@
+export const data = {
+  "name": "Warden",
+  "chain": "WARD",
+  "icon": "warden",
+  "rpc": ["https://evm.chiado.wardenprotocol.org", "wss://evm-ws.chiado.wardenprotocol.org"],
+  "nativeCurrency": {
+    "name": "WARD",
+    "symbol": "WARD",
+    "decimals": 18,
+  },
+  "infoURL": "https://wardenprotocol.org/",
+  "shortName": "ward",
+  "chainId": 8765,
+  "networkId": 8765,
+  "explorers": [
+    {
+      "name": "Warden Labs",
+      "url": "https://explorer.wardenprotocol.org",
+    },
+  ],
+};
