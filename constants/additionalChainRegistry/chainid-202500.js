2ec43d5400c7ea246b69649a0c8fd7df2cc98a48
priyanshuthapliyal55 <159440241+priyanshuthapliyal55@users.noreply.github.com>
2025-10-06 18:51:31 +0530

    new chains added (#2084)

---
+++ constants/additionalChainRegistry/chainid-202500.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Propulence Testnet",
+  "chain": "Propulence",
+  "rpc": [
+    "https://rpc.testnet.thepropulence.com"
+  ],
+  "faucets": ["https://faucet.testnet.thepropulence.com"],
+  "nativeCurrency": {
+    "name": "Propulence",
+    "symbol": "PROPX",
+    "decimals": 18
+  },
+  "shortName": "Propulence-testnet",
+  "chainId": 202500,
+  "networkId": 202500,
+  "explorers": [
+    {
+      "name": "Propulence Testnet Explorer",
+      "url": "https://explorer.testnet.thepropulence.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
