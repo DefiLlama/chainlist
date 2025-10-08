e6f809473fde12306ae45eeb173d8fe9625c3523
Sergei Vorobev <admin@vorobevsa.com>
2025-10-08 08:15:30 -0300

    Fix chain name testethiq

--- constants/additionalChainRegistry/chainid-853211.js
+++ constants/additionalChainRegistry/chainid-853211.js
@@ -1,9 +1,9 @@
 export const data = {
-  "name": "HAQQ Testethic (L2 Sepolia Testnet)",
+  "name": "HAQQ Testethiq (L2 Sepolia Testnet)",
   "chain": "ETH",
   "rpc": [
-    "https://rpc.testethic.haqq.network",
-    "wss://rpc.testethic.haqq.network"
+    "https://rpc.testethiq.haqq.network",
+    "wss://rpc.testethiq.haqq.network"
   ],
   "faucets": [
   ],
@@ -19,15 +19,15 @@ export const data = {
     { "name": "EIP4844" }
   ],
   "infoURL": "https://www.haqq.network",
-  "shortName": "haqq-testethic",
+  "shortName": "haqq-testethiq",
   "chainId": 853211,
   "networkId": 853211,
   "testnet": true,
   "icon": "haqq",
   "explorers": [
     {
-      "name": "HAQQ Testethic Blockscout",
-      "url": "https://explorer.testethic.haqq.network",
+      "name": "HAQQ Testethiq Blockscout",
+      "url": "https://explorer.testethiq.haqq.network",
       "icon": "blockscout",
       "standard": "EIP3091"
     }
