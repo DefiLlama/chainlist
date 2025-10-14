43c41e4de4d0931c44b55840d70b64486e5ae09d
ga-reth <hi@gareth-v.com>
2025-10-14 10:04:44 +0100

    Create chainid-166.js and update chainIds.js (#2060)

---
+++ constants/additionalChainRegistry/chainid-166.js
@@ -0,0 +1,26 @@
+export const data = {
+    "name": "Nomina Mainnet",
+    "chain": "NOM",
+    "icon": "https://raw.githubusercontent.com/omni-network/omni/refs/heads/main/docs/docs/public/nom/logo.png",
+    "rpc": [
+      "https://mainnet.omni.network"
+    ],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "NOM",
+      "symbol": "NOM",
+      "decimals": 18
+    },
+    "infoURL": "https://www.nomina.io",
+    "shortName": "nomina",
+    "chainId": 166,
+    "networkId": 166,
+    "explorers": [
+      {
+        "name": "Nomina Scan",
+        "url": "https://omniscan.network/",
+        "icon": "https://raw.githubusercontent.com/omni-network/omni/refs/heads/main/docs/docs/public/nom/logo.png",
+        "standard": "EIP3091"
+      }
+    ]
+  }
