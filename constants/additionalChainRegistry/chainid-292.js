bc909086a8698fd867dcd5dcd351eac75079197e
djsplice <barrows.jeff@gmail.com>
2025-10-24 16:32:38 -0700

    Add ChainID 292 for HashSphere

---
+++ constants/additionalChainRegistry/chainid-292.js
@@ -0,0 +1,17 @@
+{
+    "name": "HashSphere",
+    "chain": "HashSphere",
+    "rpc": [],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "hbar",
+      "symbol": "HBAR",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "infoURL": "https://hashgraph.com",
+    "shortName": "hbar",
+    "chainId": 292,
+    "networkId": 292,
+    "explorers": []
+  }
