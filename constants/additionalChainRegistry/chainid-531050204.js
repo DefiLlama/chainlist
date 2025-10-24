a714aa8c7addeacfa14e2089f4d177e3296a1f7f
Maksim Krukovich <pl.maksim.krukovich@gmail.com>
2025-10-24 12:46:58 +0200

    Add: Sophon zkSync-OS Testnet (#2138)
    * Add: Sophon zkSync-OS Testnet
    
    * remove sophon chain id from constants

---
+++ constants/additionalChainRegistry/chainid-531050204.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Sophon zkSync-OS Testnet",
+  "chain": "Sophon",
+  "rpc": [
+    "https://zksync-os-testnet-sophon.zksync.dev/",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Sophon",
+    "symbol": "SOPH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://sophon.xyz/",
+  "shortName": "sophon-testnet",
+  "chainId": 531050204,
+  "networkId": 531050204,
+  "explorers": [{
+    "name": "Sophon zkSync Testnet Explorer",
+    "url": "https://block-explorer.zksync-os-testnet-sophon.zksync.dev/",
+    "standard": "EIP3091"
+  }]
+}
