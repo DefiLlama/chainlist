9e263453449067a2a3cc71c27d3c2a4bd75b9e13
Mangosago <mangosago@xone.org>
2025-10-25 12:07:03 +0700

    Add: Xone Mainnet and Testnet info

---
+++ constants/additionalChainRegistry/chainid-33772211.js
@@ -0,0 +1,30 @@
+export const data = {
+  "name": "Xone Testnet",
+  "chain": "XONE TESTNET",
+  "rpc": [
+    "https://rpc-testnet.xone.plus",
+    "https://rpc-testnet.xone.org",
+    "https://rpc-testnet.knight.center",
+    "wss://rpc-testnet.xone.org"
+  ],
+  "faucets": ["https://faucet.xone.org/"],
+  "nativeCurrency": {
+    "name": "Xone Coin",
+    "symbol": "XOC",
+    "decimals": 18
+  },
+  "infoURL": "https://xone.org",
+  "shortName": "xoc",
+  "chainId": 33772211,
+  "networkId": 33772211,
+  "icon": "xone",
+  "slip44": 1,
+  "explorers": [
+    {
+      "name": "Xone Testnet Explorer",
+      "url": "https://testnet.xonescan.com/",
+      "icon": "xone",
+      "standard": "EIP3091",
+    }
+  ]
+};
---
+++ constants/additionalChainRegistry/chainid-3721.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "Xone Mainnet",
+  "chain": "XONE MAINNET",
+  "rpc": [
+    "https://rpc.xone.org",
+    "wss://rpc.xone.org"
+  ],
+  "faucets": ["https://faucet.xone.org/"],
+  "nativeCurrency": {
+    "name": "Xone Coin",
+    "symbol": "XOC",
+    "decimals": 18
+  },
+  "infoURL": "https://xone.org",
+  "shortName": "xoc",
+  "chainId": 3721,
+  "networkId": 3721,
+  "icon": "xone",
+  "slip44": 1,
+  "explorers": [
+    {
+      "name": "Xone Mainnet Explorer",
+      "url": "https://xonescan.com/",
+      "icon": "xone",
+      "standard": "EIP3091",
+    }
+  ]
+};
