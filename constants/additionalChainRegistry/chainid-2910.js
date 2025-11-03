5a9d4889c03866fa3d37d678cdb08d3ed2c4f536
Segue <huoda.china@163.com>
2025-11-03 23:41:31 +0800

    add Morph Hoodi Testnet

---
+++ constants/additionalChainRegistry/chainid-2910.js
@@ -0,0 +1,24 @@
+export const data = {
+  name: "Morph Hoodi",
+  chain: "ETH",
+  rpc: ["https://rpc-hoodi.morphl2.io"],
+  faucets: [],
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://www.morphl2.io",
+  shortName: "morph",
+  chainId: 2910,
+  networkId: 2910,
+  icon: "https://icons.llamao.fi/icons/chains/rsz_morph.jpg",
+  explorers: [
+    {
+      name: "Morph Hoodi Testnet Explorer",
+      url: "https://explorer-hoodi.morphl2.io",
+      standard: "EIP3091",
+    },
+  ],
+};
