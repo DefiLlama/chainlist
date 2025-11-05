3eba3422d43f129a0536eb24eed12f5b0a6bcec5
Sergi Solvez <sssolvez@gmail.com>
2025-11-05 10:47:41 +0900

    Add JASMY Chain

---
+++ constants/additionalChainRegistry/chainid-680.js
@@ -0,0 +1,18 @@
+export const data = {
+  name: "JASMY Chain",
+  chain: "JASMY",
+  rpc: [],
+  faucets: [],
+  nativeCurrency: {
+    name: "JasmyCoin",
+    symbol: "JASMY",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://www.jasmy.co.jp/en.html",
+  shortName: "jasmy",
+  chainId: 680,
+  networkId: 680,
+  icon: "jasmy",
+  explorers: [],
+};
