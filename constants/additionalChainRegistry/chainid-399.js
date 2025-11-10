71df323e65e5433ab8d063d8251d38af1359329b
tianyi <tianyi@vip.com>
2025-11-10 21:04:45 +0800

    add nadi chain

---
+++ constants/additionalChainRegistry/chainid-399.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "NadiDepin Mainnet",
+  "chain": "NADI",
+  "rpc": [
+    "https://rpc.naaidepin.co",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "NADI",
+    "symbol": "NADI",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://www.naaidepin.co",
+  "shortName": "nadi",
+  "chainId": 399,
+  "networkId": 399,
+  "icon": "naaidepin",
+  "explorers": [{
+    "name": "naaidepin",
+    "url": "https://explorer.naaidepin.co",
+    "icon": "naaidepin",
+    "standard": "EIP3091"
+  }]
+}
