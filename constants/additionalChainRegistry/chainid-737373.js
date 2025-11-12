b6833bec30d2ec5957d8a0e25016eb745ad8086d
ArrayCodes <araycik@gmail.com>
2025-11-12 16:41:49 +0300

    fix(chain-registry): change logo CX Chain Mainnet and Testnet

--- constants/additionalChainRegistry/chainid-737373.js
+++ constants/additionalChainRegistry/chainid-737373.js
@@ -1,7 +1,7 @@
 export const data = {
   "name": "CX Chain Mainnet",
   "chain": "CX",
-  "icon": "ipfs://bafkreiemtyffyyodqiqxinaejvytbzcfn4n572waybnpknzzehtawumrla",
+  "icon": "https://images.ctfassets.net/gcj8jwzm6086/3wVuWA4oz9iMadkIpywUMM/377249d5b8243e4dfa3a426a1af5eaa5/14.png",
   "rpc": [
     "https://subnets.avax.network/cx/mainnet/rpc",
   ],
@@ -15,7 +15,6 @@ export const data = {
   "shortName": "cx",
   "chainId": 737373,
   "networkId": 737373,
-  "icon": "cxchain",
   "explorers": [{
     "name": "CX Chain Mainnet Explorer",
     "url": "https://subnets.avax.network/cx",
