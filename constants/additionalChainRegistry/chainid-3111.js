961c9006873c6789cf800a5b7e7955dcd2e17c38
Daniel Zhou <daniel.zhou@bitlayer.ltd>
2025-11-12 15:08:08 +0800

    Add Alpha Chain to Chainlist (3111 & 511111)

---
+++ constants/additionalChainRegistry/chainid-3111.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Alpha Chain Mainnet",
+  "chain": "Alpha Chain",
+  "rpc": [
+    "https://rpc.goalpha.org"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://docs.alphatoken.com/AlphaChain/about-alpha-chain",
+  "shortName": "alpha",
+  "chainId": 3111,
+  "networkId": 3111,
+  "explorers": [{
+    "name": "Alpha Chain Scan",
+    "url": "https://scan.goalpha.org",
+    "standard": "EIP3091"
+  }]
+}
