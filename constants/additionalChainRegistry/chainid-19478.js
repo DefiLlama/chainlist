469dee3b1237777269bbf7f28eb6ee1ac03717fe
Surou <cppfuns@gmail.com>
2025-11-20 23:38:23 +0800

    add# Trustivon

---
+++ constants/additionalChainRegistry/chainid-19478.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "Trustivon Testnet",
+  "chain": "Trustivon",
+  "shortName": "trustivon",
+  "infoURL": "https://trustivon.com",
+  "icon": "trustivon",
+  "chainId": 19478,
+  "networkId": 19478,
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "Trustivon",
+    "symbol": "TC",
+    "decimals": 18
+  },
+  "rpc": ["https://rpc.trustivon.com"],
+  "faucets": [],
+  "explorers": [
+    {
+      "name": "Trustivon Explorer",
+      "url": "https://scan.trustivon.com",
+      'logo': 'blockscout',
+      "standard": "EIP3091"
+    }
+  ]
+}
