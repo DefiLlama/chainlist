f9cf478342d4bc497d2af0bf7bf2c99932ba2704
lorem <lorem.ipsum.crypto@gmail.com>
2025-11-22 14:39:32 -0500

    feat: added table for chain explorers, added link to home page to logo, created separate component file for CopyUrl, added translations for new ExplorerList component

---
+++ components/ExplorerList/index.js
@@ -0,0 +1,48 @@
+import { notTranslation as useTranslations } from "../../utils";
+import CopyUrl from "../CopyUrl";
+
+export default function ExplorerList({ chain, lang }) {
+  const t = useTranslations("Common", lang);
+  const explorerLinks = chain.explorers;
+
+  return (
+    <div className="shadow dark:bg-[#0D0D0D] bg-white p-8 rounded-[10px] flex flex-col gap-3 col-span-full relative overflow-x-auto">
+      <table className="m-0 border-collapse whitespace-nowrap dark:text-[#B3B3B3] text-black">
+        <caption className="relative w-full px-3 py-1 text-base font-medium border border-b-0">
+          <span className="mr-4">{`${chain.name} ${t("explorer-url-list")}`}</span>
+        </caption>
+        <thead>
+          <tr>
+            <th className="px-3 py-1 font-medium border">{t("explorer-name")}</th>
+            <th className="px-3 py-1 font-medium border">{t("explorer-url")}</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {explorerLinks.map((explorer, index) => {
+            let className = "bg-inherit";
+            return (
+              <ExplorerRow
+                isLoading={chain.isLoading}
+                explorer={explorer}
+                key={"explorer" + index}
+                className={className}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+const ExplorerRow = ({ isLoading, explorer, className }) => {
+  return (
+    <tr className={className}>
+      <td className="px-3 py-1 text-sm border text-center">{isLoading ? <Shimmer /> : explorer?.name}</td>
+      <td className="border px-3 py-1 max-w-[40ch] text-center">
+        {isLoading ? <Shimmer /> : explorer?.url ? <CopyUrl url={explorer.url} /> : null}
+      </td>
+    </tr>
+  );
+};
