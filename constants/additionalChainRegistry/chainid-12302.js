0abd8ac241c18e06981515321f309709ddd351fe
hundanwan <60227093+hundanwan@users.noreply.github.com>
2025-11-24 17:50:24 +0800

    add chainid 12301 and 12302 (#2241)

---
+++ constants/additionalChainRegistry/chainid-12302.js
@@ -0,0 +1,23 @@
+export const data = {
+  name: "CTC Chain Testnet",
+  chain: "CTC",
+  icon: "ctcchain",
+  rpc: ["https://test-rpc.tantin.com"],
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  faucets: [],
+  nativeCurrency: {
+    name: "CTC",
+    symbol: "CTC",
+    decimals: 18,
+  },
+  infoURL: "https://tantin.com/",
+  shortName: "ctcchain",
+  chainId: 12302,
+  networkId: 12302,
+  explorers: [
+    {
+      name: "CTC Chain Testnet Explorer",
+      url: "https://test-scan.tantin.com",
+    },
+  ]
+};
