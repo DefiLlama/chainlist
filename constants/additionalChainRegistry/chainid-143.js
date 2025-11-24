2d738e85fdcc0a193985171c29219fc11740dcea
HBD Chain <110482911+hbdchain@users.noreply.github.com>
2025-11-25 00:45:48 +0530

    Add HBD Chain

---
+++ constants/additionalChainRegistry/chainid-143.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "HBD Chain",
+  "chain": "HBD",
+  "icon": "hbd",
+  "rpc": [
+    "https://rpc.hbdscan.com",
+  ],
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "HBD Chain",
+    "symbol": "HBD",
+    "decimals": 18
+  },
+  "infoURL": "https://www.hbdchain.com",
+  "shortName": "hbd",
+  "chainId": 143,
+  "networkId": 143,
+  "explorers": [
+    {
+      "name": "HBD Scan",
+      "url": "https://hbdscan.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
