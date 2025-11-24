ef9683e27dfb62d0f181fa7fee2be3d1f5d5214e
Sebastián Galiano <sgaliano@gmail.com>
2025-11-24 20:23:26 -0300

    Add DogeOS Chikyū Testnet (Chain ID: 6281971)

---
+++ constants/additionalChainRegistry/chainid-6281971.js
@@ -0,0 +1,29 @@
+export const data = {
+  name: "DogeOS Chikyū Testnet",
+  chain: "DogeOS",
+  rpc: [
+    "https://rpc.testnet.dogeos.com",
+  ],
+  faucets: [
+    "https://faucet.testnet.dogeos.com",
+  ],
+  nativeCurrency: {
+    name: "Dogecoin",
+    symbol: "DOGE",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://www.dogeos.com",
+  shortName: "dogeos-chykyu",
+  chainId: 6281971,
+  networkId: 6281971,
+  icon: "dogeos",
+  explorers: [
+    {
+      name: "DogeOS Chikyū Testnet Explorer",
+      url: "https://blockscout.testnet.dogeos.com",
+      icon: "blockscout",
+      standard: "EIP3091",
+    },
+  ],
+};
