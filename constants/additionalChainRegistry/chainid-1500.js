45e37334396bfdaf5b16fe65a0f0d23b256a7916
VanjaRo <vanjaro43@gmail.com>
2025-11-27 17:12:59 +0300

    add midl

---
+++ constants/additionalChainRegistry/chainid-1500.js
@@ -0,0 +1,19 @@
+export const data = {
+  "name": "Midl Mainnet",
+  "chain": "MIDL",
+  "rpc": [
+    "https://rpc.midl.org",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Bitcoin",
+    "symbol": "BTC",
+    "decimals": 18
+  },
+  "infoURL": "https://midl.xyz/",
+  "shortName": "midl",
+  "chainId": 1500,
+  "networkId": 1500,
+  "explorers": []
+}
+
