d793019daf23fabe8480667b2628fa3d6858fe1e
Ryder Hiew <ryderhiew@Ryders-MacBook-Pro-2.local>
2025-11-28 20:51:49 +0800

    Add HyperX chain (888)

---
+++ constants/additionalChainRegistry/chainid-888.js
@@ -0,0 +1,27 @@
+export default {
+  "name": "HyperX",
+  "chain": "HPX",
+  "rpc": [
+    "https://rpc.hyperx.technology"
+  ],
+  "faucets": [
+    "https://faucet.hyperx.technology"
+  ],
+  "nativeCurrency": {
+    "name": "HPX",
+    "symbol": "HPX",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://hyperx.technology",
+  "shortName": "hpx",
+  "chainId": 888,
+  "networkId": 888,
+  "explorers": [
+    {
+      "name": "HyperX Explorer",
+      "url": "https://scan.hyperx.technology",
+      "standard": "EIP3091"
+    }
+  ]
+}
