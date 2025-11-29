efe65a2bfc1a01f5f67d7774abbc8543f268d795
caranell <caranell@proton.me>
2025-11-29 07:36:39 +0300

    Sort testnets by tvl (#2173)
    * sort testnets by mainnet' tvl
    
    * Fix show all button visibility
    
    * Fix builds
    
    * Handle post-export script failure in build.sh
    
    Updates build.sh to check the exit status of post-export.sh. If post-export fails but the build succeeded, the build status is updated to reflect the failure, ensuring accurate reporting of build results.
    
    * fix build
    
    * Fix deprecated import
    
    * Revert "Fix deprecated import"
    
    This reverts commit 35c83f2dc97489ed26b810235ffe685e9ecfe969.
    
    * refactor
    
    * exit build if post export script fails
    
    ---------
    
    Co-authored-by: mintdart <96025197+mintdart@users.noreply.github.com>

--- scripts/post-export.sh
+++ scripts/post-export.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+set -e
 
-sed -i '4 i "type": "module",' package.json
 node generate-sitemap.js
 node generate-json.js
-sed -i "4 d" package.json
+
 rm out/404.html
 mv out/error.html out/404.html
 cp serve.json out/serve.json
