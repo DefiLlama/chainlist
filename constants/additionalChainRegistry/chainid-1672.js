1024768543c253d156fe3f443ea6516a7dac13f3
oswald <oswald@oswalddeMacBook-Air.local>
2025-12-13 21:07:19 +0800

    add pharos mainnet

---
+++ constants/additionalChainRegistry/chainid-1672.js
@@ -0,0 +1,24 @@
+export const data = {
+    "name": "Pharos Mainnet",
+    "title": "Pharos Mainnet",
+    "chain": "Pharos",
+    "icon": "pharosmainnet",
+    "rpc": ["https://rpc.pharos.xyz"],
+    "faucets": [],
+    "nativeCurrency": {
+        "name": "PharosCoin",
+        "symbol": "PROS",
+        "decimals": 18
+    },
+    "infoURL": "https://pharos.xyz/",
+    "shortName": "pharos",
+    "chainId": 1672,
+    "networkId": 1672,
+    "explorers": [
+        {
+            "name": "Pharos Explorer",
+            "url": "https://pharosscan.xyz",
+            "standard": "EIP3091"
+        }
+    ]
+}
