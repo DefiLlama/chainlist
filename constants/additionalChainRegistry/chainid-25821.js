e30c55ed9d2e546a1eca762d623fd8b369ce2a70
vangogh <49611950+vangoghl@users.noreply.github.com>
2025-12-15 21:47:07 +0800

    Add H2 Mainnet (2582) and H2 Lambda (25821) (#2333)
    Co-authored-by: vangogh0229 <vangogh0229@gmail.com>

---
+++ constants/additionalChainRegistry/chainid-25821.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "H2 Lambda",
+  "chain": "H2",
+  "rpc": [
+    "https://rpc.h-1.io"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "H2",
+    "symbol": "H2",
+    "decimals": 18
+  },
+  "infoURL": "https://h2chain.io",
+  "shortName": "h2lambda",
+  "chainId": 25821,
+  "networkId": 25821,
+  "icon": "https://h2inno.com/logo.svg",
+  "explorers": [
+    {
+      "name": "H2Scan",
+      "url": "https://lambda.h2scan.io",
+      "standard": "EIP3091"
+    }
+  ]
+}
+
