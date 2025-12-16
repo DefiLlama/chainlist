e02aca7d22cde8f9e8195bfa0aa93198cb9c5dab
freddi8e <freddi8e@gmail.com>
2025-12-16 13:47:51 +0800

    cpchain

---
+++ constants/additionalChainRegistry/chainid-86606.js
@@ -0,0 +1,28 @@
+export const data = {
+    "name": "Cp Chain Testnet",
+    "chain": "CP",
+    "icon": "cp",
+    "rpc": [
+        "https://rpc-testnet.cpchain.com",
+    ],
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "faucets": [
+        "https://www.cpchain.com/faucet"
+    ],
+    "nativeCurrency": {
+        "name": "cp",
+        "symbol": "CP",
+        "decimals": 18
+    },
+    "infoURL": "https://www.cpchain.com/",
+    "shortName": "cp",
+    "chainId": 86606,
+    "networkId": 86606,
+    "explorers": [
+        {
+            "name": "CpChainExplorer",
+            "url": "https://explorer-testnet.cpchain.com/",
+            "standard": "EIP3091"
+        },
+    ]
+}
---
+++ constants/additionalChainRegistry/chainid-86608.js
@@ -0,0 +1,26 @@
+export const data = {
+    "name": "Cp Chain Mainnet",
+    "chain": "CP",
+    "icon": "cp",
+    "rpc": [
+        "https://rpc.cpchain.com",
+    ],
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "faucets": [],
+    "nativeCurrency": {
+        "name": "cp",
+        "symbol": "CP",
+        "decimals": 18
+    },
+    "infoURL": "https://www.cpchain.com/",
+    "shortName": "cp",
+    "chainId": 86608,
+    "networkId": 86608,
+    "explorers": [
+        {
+            "name": "CpChainExplorer",
+            "url": "https://explorer.cpchain.com/",
+            "standard": "EIP3091"
+        },
+    ]
+}
