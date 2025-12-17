e329398bb9c4845c9934d1cea276b0360f89cf33
jeongho7yeo <jeongho7.yeo@gmail.com>
2025-12-17 10:47:05 +0900

    Add Ault Blockchain mainnet and testnet

---
+++ constants/additionalChainRegistry/chainid-10904.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "Ault Blockchain Testnet",
+  chain: "AULT",
+  rpc: [],
+  faucets: [],
+  nativeCurrency: {
+    name: "Testnet AULT Token",
+    symbol: "AULT",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://aultblockchain.com",
+  shortName: "ault-testnet",
+  chainId: 10904,
+  networkId: 10904,
+  icon: "ault",
+  explorers: [],
+};
---
+++ constants/additionalChainRegistry/chainid-904.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "Ault Blockchain Mainnet",
+  chain: "AULT",
+  rpc: [],
+  faucets: [],
+  nativeCurrency: {
+    name: "AULT Token",
+    symbol: "AULT",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://aultblockchain.com",
+  shortName: "ault",
+  chainId: 904,
+  networkId: 904,
+  icon: "ault",
+  explorers: [],
+};
