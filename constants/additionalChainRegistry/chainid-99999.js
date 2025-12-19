a46fcac07bcb3e067329cead5e52d4e04dc4cf80
Oleksandr Torbiievskyi <alex.t@ideasoft.io>
2025-12-19 10:54:48 +0200

    feat(): add ADI mainnet (ID: 36900) and testnet (ID: 99999)

---
+++ constants/additionalChainRegistry/chainid-99999.js
@@ -0,0 +1,31 @@
+export const data = {
+  "name": "ADI Testnet",
+  "chain": "ADI",
+  "rpc": [
+    "https://rpc.ab.testnet.adifoundation.ai"
+  ],
+  "faucets": ["https://faucet.ab.testnet.adifoundation.ai"],
+  "nativeCurrency": {
+    "name": "ADI Token",
+    "symbol": "ADI",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://adi.foundation",
+  "shortName": "adi-testnet",
+  "chainId": 99999,
+  "networkId": 99999,
+  "icon": "adi",
+  "explorers": [
+    {
+      "name": "ADI Testnet Explorer",
+      "url": "https://explorer.ab.testnet.adifoundation.ai",
+      "standard": "EIP3091"
+    },
+    {
+      "name": "ADI Testnet Blockscout Explorer",
+      "url": "https://blockscout.ab.testnet.adifoundation.ai",
+      "standard": "EIP3091"
+    }
+  ]
+}
