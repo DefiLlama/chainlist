58162f38c48d3a830817c4502a0a72da6f11d3ba
entaiafrica <enterai.africa@gmail.com>
2025-12-24 10:07:50 +0200

    Add files via upload

---
+++ constants/additionalChainRegistry/chainid-12786.js
@@ -0,0 +1,29 @@
+export const data = {
+  "name": "BrickChain",
+  "chain": "BRKG",
+  "rpc": [
+    "https://rpc.firstbrick.cloud"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Brick Gas",
+    "symbol": "BRKG",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://firstbrick.cloud",
+  "shortName": "brick",
+  "chainId": 12786,
+  "networkId": 12786,
+  "icon": "brickchain",
+  "explorers": [
+    {
+      "name": "BrickChain Explorer",
+      "url": "https://explorer.firstbrick.cloud",
+      "standard": "EIP3091"
+    }
+  ],
+}
