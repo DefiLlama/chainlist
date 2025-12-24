3164e100e75ef3b1e42362169597bbb8abbc6937
bxpana <ba@matterlabs.dev>
2025-12-24 09:43:18 -0500

    feat: add creator chain testnet
    - Add Creator Chain Testnet to the chainlist
    - This Creator chain is deployed with the ZK Stack

---
+++ constants/additionalChainRegistry/chainid-278701.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "Creator Chain Testnet",
+    "chain": "Creator Chain",
+    "rpc": [
+      "https://zksync-os-testnet-creator.zksync.dev",
+    ],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Ether",
+      "symbol": "ETH",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "infoURL": "https://x.com/oncreator_",
+    "shortName": "creator-chain-testnet",
+    "chainId": 278701,
+    "networkId": 278701,
+    "explorers": [
+      {
+        "name": "Creator Chain Testnet Explorer",
+        "url": "https://block-explorer.zksync-os-testnet-creator.zksync.dev",
+        "standard": "EIP3091"
+      }
+    ]
+  }
