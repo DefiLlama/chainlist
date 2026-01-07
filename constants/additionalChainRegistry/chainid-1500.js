50cad6c6ce23007f45731351e1554da21a271078
Ivan Rozhnovskiy <43208710+VanjaRo@users.noreply.github.com>
2026-01-07 19:19:01 +0300

    add midl (#2279)
    * add midl
    
    * fix: changed testnet id
    
    * fix: added explorers
    
    * fix: changed domen

---
+++ constants/additionalChainRegistry/chainid-1500.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "Midl Mainnet",
+  "chain": "MIDL",
+  "rpc": [
+    "https://rpc.midl.xyz",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Bitcoin",
+    "symbol": "BTC",
+    "decimals": 18
+  },
+  "infoURL": "https://midl.xyz/",
+  "shortName": "midl",
+  "chainId": 1500,
+  "networkId": 1500,
+  "explorers": [
+    {
+      "name": "Midl Mainnet Explorer",
+      "url": "blockscout.midl.xyz",
+      "icon": "midl",
+      "standard": "EIP3091"
+    }
+  ]
+}
+
