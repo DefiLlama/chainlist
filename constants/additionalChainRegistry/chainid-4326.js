9d70e394225ccd972bd3fec03a7ecd47c2963368
B̾R̾E̾A̾D̾ <97642356+0xBreadguy@users.noreply.github.com>
2026-01-14 15:03:00 -0800

    Create chainid-4326.js

---
+++ constants/additionalChainRegistry/chainid-4326.js
@@ -0,0 +1,30 @@
+export const data = {
+  "name": "MegaETH",
+  "chain": "MEGA",
+  "icon": "megaeth",
+  "rpc": [
+    "https://mainnet.megaeth.com/rpc",
+  ],
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "infoURL": "https://www.megaeth.com",
+  "shortName": "megaeth",
+  "chainId": 4326,
+  "networkId": 4326,
+  "explorers": [
+    {
+      "name": "Blockscout",
+      "url": "https://megaeth.blockscout.com",
+      "standard": "EIP3091"
+    },
+    {
+      "name": "Etherscan",
+      "url": "https://mega.etherscan.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
