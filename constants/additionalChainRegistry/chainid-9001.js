5610171b964257407de097923f748d2a04c81d27
Regal <admin@theyuusystem.com>
2026-01-19 12:11:42 +0000

    Add YuuChain (chainId 9001)

---
+++ constants/additionalChainRegistry/chainid-9001.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "YuuChain",
+  chain: "YUU",
+  rpc: [
+    "https://rpc.theyuusystem.com",
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Yuu",
+    symbol: "YUU",
+    decimals: 18,
+  },
+  infoURL: "https://theyuusystem.com",
+  shortName: "yuuchain",
+  chainId: 9001,
+  networkId: 9001,
+  icon: "yuuchain",
+  explorers: [
+    {
+      name: "YuuChain Explorer",
+      url: "https://explorer.theyuusystem.com",
+      standard: "EIP3091",
+    },
+  ],
+}
