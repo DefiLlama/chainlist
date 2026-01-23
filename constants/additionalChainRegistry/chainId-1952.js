00f6059d8140da9bb0c59703f4c68021535f68fc
realshaman <realdealshaman11@gmail.com>
2026-01-23 07:10:15 -0600

    add new additionalChainRegistry file and fix existing

---
+++ constants/additionalChainRegistry/chainId-1952.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "X Layer Testnet",
+    "chain": "X Layer",
+    "icon": "x layer",
+    "rpc": [
+      "https://testrpc.xlayer.tech", "https://xlayertestrpc.okx.com"
+    ],
+    "faucets": ["https://www.okx.com/xlayer/faucet"],
+    "nativeCurrency": {
+      "name": "X Layer Global Utility Token in testnet",
+      "symbol": "OKB",
+      "decimals": 18
+    },
+    "infoURL": "https://www.okx.com/xlayer",
+    "shortName": "tokb",
+    "chainId": 1952,
+    "networkId": 1952,
+    "explorers": [
+      {
+        "name": "OKLink",
+        "url": "https://www.oklink.com/xlayer-test",
+        "standard": "EIP3091"
+      }
+    ]
+  }
