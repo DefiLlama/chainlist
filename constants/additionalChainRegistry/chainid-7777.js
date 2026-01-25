d751552c75978b12513e198e9848a59c162ee9f1
satlykkarakulow09-collab <satlykkarakulow09@gmail.com>
2026-01-25 21:25:32 +0300

    Create chainid-7777.js
    Add BNX Smart Chain (chainId 7777)

---
+++ constants/additionalChainRegistry/chainid-7777.js
@@ -0,0 +1,25 @@
+module.exports = {
+  name: "BNX Smart Chain",
+  chain: "BNX",
+  rpc: [
+    "http://217.76.56.234:8545"
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "BNX",
+    symbol: "BNX",
+    decimals: 18
+  },
+  infoURL: "https://betnetx.xyz",
+  shortName: "bnx",
+  chainId: 7777,
+  networkId: 7777,
+  icon: "bnx",
+  explorers: [
+    {
+      name: "BNX Explorer",
+      url: "http://217.76.56.234:8545",
+      standard: "EIP3091"
+    }
+  ]
+}
