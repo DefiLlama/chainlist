e45ce51d665f786f77180a5cf0dad0534860fc1b
mkd951 <mukundmishra0505@gmail.com>
2026-02-02 18:49:53 +0530

    Add chainid-1330.js

---
+++ constants/additionalChainRegistry/chainid-1330.js
@@ -0,0 +1,25 @@
+export const data = {
+  name: “ONNChain”,
+  chain: “onn”,
+  rpc: [
+    "https://mainnet.onnscan.org",
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: “ONNCoin”,
+    symbol: “ONN”,
+    decimals: 18,
+  },
+  infoURL: "https://onnscan.com",
+  shortName: “onn”,
+  chainId: 1330,
+  networkId: 1330,
+  icon: “onn”,
+  explorers: [
+    {
+      name: “ONNChain Explorer",
+      url: "https://onnscan.com",
+      standard: "EIP3091",
+    },
+  ],
+}
