fb145768ca87b687307036b3db0b0178a4ed0970
Your Name <you@example.com>
2026-02-04 16:36:09 +0530

    feat: add MSC chain 5577 to additionalChainRegistry

---
+++ constants/additionalChainRegistry/chainid-5577.js
@@ -0,0 +1,19 @@
+﻿export const chain5577 = {
+  "name": "MSC Blockchain",
+  "chain": "MSC",
+  "rpc": [
+    "https://rpc.taaqo.com"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "MSC Token",
+    "symbol": "MSC",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://taaqo.com",
+  "shortName": "msc",
+  "chainId": 5577,
+  "networkId": 5577,
+  "explorers": []
+}
