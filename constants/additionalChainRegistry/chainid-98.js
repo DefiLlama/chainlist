785e4b2b9c6ff5e34b739bb0a8d61014e79af8ff
Hossein Pourbahrami <hossein.pourbahrami@gmail.com>
2026-02-09 01:36:19 +0330

    Create chainid-98.js

---
+++ constants/additionalChainRegistry/chainid-98.js
@@ -0,0 +1,22 @@
+{
+  "name": "ION Testnet",
+  "chain": "ION",
+  "rpc": [
+    "https://ion.qaflanti.ir/api/rpc",
+  ],
+  "faucets": [
+    "https://ion.qaflanti.ir/api/faucet"
+  ],
+  "nativeCurrency": {
+    "name": "ION",
+    "symbol": "ION",
+    "decimals": 18
+  },
+  "features": [],
+  "infoURL": "https://ion.qaflanti.ir",
+  "shortName": "ion",
+  "chainId": 98,
+  "networkId": 98,
+  "icon": "https://ion.qaflanti.ir/icon.png",
+  "explorers": []
+}
