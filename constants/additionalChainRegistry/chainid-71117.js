b30ec7e4f052aeac4ee7bfcd3a688ef55247cbfc
Tripsy <102474163+tripsy-eth@users.noreply.github.com>
2026-02-12 18:25:11 +0530

    Replace old WadzChain to Rebranded W Chain (chainid-171717.js and chainid-71117.js)

---
+++ constants/additionalChainRegistry/chainid-71117.js
@@ -0,0 +1,25 @@
+{
+    "name": "W Chain Testnet",
+        "chain": "WCN",
+            "rpc": [
+                "https://rpc-testnet.w-chain.com"
+            ],
+                "faucets": [],
+                    "nativeCurrency": {
+        "name": "W Coin",
+            "symbol": "WCO",
+                "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+        "infoURL": "https://w-chain.com",
+            "shortName": "wcn-testnet",
+                "chainId": 71117,
+                    "networkId": 71117,
+                        "icon": "https://apricot-deep-swordtail-337.mypinata.cloud/ipfs/bafkreifnbotmhobqceuj5ghefo76gherr2pjugc2g6jssy4w5dlexqgvue",
+                            "explorers": [{
+                                "name": "W Scan Testnet",
+                                "url": "https://scan-testnet.w-chain.com",
+                                "icon": "https://apricot-deep-swordtail-337.mypinata.cloud/ipfs/bafkreifnbotmhobqceuj5ghefo76gherr2pjugc2g6jssy4w5dlexqgvue",
+                                "standard": "EIP3091"
+                            }]
+}
