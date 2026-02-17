4ad4b0fdbdbf6f06a6a90efe8ee1bbd5ec8e01de
Chip <32240939+mrchipdev@users.noreply.github.com>
2026-02-17 22:55:58 +0700

    Create chainid-12082025.js
    Add Onfa Chain Info

---
+++ constants/additionalChainRegistry/chainid-12082025.js
@@ -0,0 +1,25 @@
+{
+  "name": "ONFA Chain",
+  "chain": "OFC",
+  "rpc": [
+    "https://rpc.onfachain.com","https://rpc.onfachain.net"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "ONFA Coin",
+    "symbol": "OFC",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://onfachain.com",
+  "shortName": "ofc",
+  "chainId": 12082025,
+  "networkId": 12082025,
+  "icon": "onfacoin",
+  "explorers": [{
+    "name": "onfascan",
+    "url": "https://onfascan.io",
+    "icon": "onfascan",
+    "standard": "EIP3091"
+  }]
+}
