8c3d843c675fdc6d50d43fced920a5fec2620e5b
ZugChainLabs <87272064+ZugChainLabs@users.noreply.github.com>
2026-03-01 02:41:38 +0430

    Create chainid-824642.js
    Add ZugChain Testnet

---
+++ constants/additionalChainRegistry/chainid-824642.js
@@ -0,0 +1,24 @@
+{
+  "name": "ZugChain Testnet", 
+  "chain": "ZUG",
+  "rpc": [
+    "https://rpc.zugchain.org" // Kendi RPC URL'nizi buraya yazın
+  ],
+  "faucets": [
+    "https://testnet.zugchain.org/faucet" // Varsa Faucet adresiniz
+  ],
+  "nativeCurrency": {
+    "name": "ZUG",
+    "symbol": "ZUG",
+    "decimals": 18
+  },
+  "infoURL": "https://zugchain.org", // Ana web siteniz
+  "shortName": "zug",
+  "chainId": 824642, // ZUG Chain Testnet/Mainnet ID'niz (Dosya adıyla aynı olmalı)
+  "networkId": 824642, // Genelde Chain ID ile aynıdır
+  "explorers": [{
+    "name": "ZugChain Explorer",
+    "url": "https://explorer.zugchain.org", // Block Explorer URL'niz
+    "standard": "EIP3091"
+  }]
+}
