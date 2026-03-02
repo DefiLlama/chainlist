1f522bc412af0e16d24dc87617f0e372ce5bf2c1
FiveKG <48405099+FiveKG@users.noreply.github.com>
2026-03-02 20:24:29 +0800

    Added onfinality zksync seplia endpoint (#2534)
    * Add OnFinality RPC endpoint for optimism-sepolia
    
    * Add OnFinality RPC endpoint for ZKsync Sepolia

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -4355,6 +4355,11 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.ankr,
       },
+      {
+        url: "https://zksync-sepolia.api.onfinality.io/public",
+        tracking: "limited",
+        trackingDetails: privacyStatement.onfinality,
+      },
     ],
   },
   324: {
