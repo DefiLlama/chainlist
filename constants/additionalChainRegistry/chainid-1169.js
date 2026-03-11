2d62ee381e3b947bdc2084073a1669cd3bb6b1b5
D3v0rs0 <35398477+devorso@users.noreply.github.com>
2026-03-11 13:02:40 +0100

    Add Solarius Chain Mainnet configuration

---
+++ constants/additionalChainRegistry/chainid-1169.js
@@ -0,0 +1,27 @@
+{
+  "name": "Solarius Chain Mainnet",
+  "chain": "SOLU",
+  "rpc": [
+    "https://mainnet-1.solariuschain.org",
+    "https://mainnet-2.solariuschain.org",
+    "https://mainnet-3.solariuschain.org"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Solarius Coin",
+    "symbol": "SOLU",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://solariuschain.org",
+  "shortName": "sol",
+  "chainId": 1,
+  "networkId": 1,
+  "icon": "ethereum",
+  "explorers": [{
+    "name": "Solarius Explorer",
+    "url": "https://explorer.solariuschain.org",
+    "icon": "solariusscan",
+    "standard": "EIP3091"
+  }]
+}
