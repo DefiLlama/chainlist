ed52e32d21210848304d0eedd7eea3aae7f8515d
Scott Canning <scott@Mac.lan>
2026-03-17 14:18:49 -0400

    add Fluent mainnet

---
+++ constants/additionalChainRegistry/chainid-25363.js
@@ -0,0 +1,23 @@
+export const data = {
+  name: "Fluent",
+  chain: "FLUENT",
+  rpc: ["https://rpc.fluent.xyz"],
+  faucets: [],
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://www.fluent.xyz/",
+  shortName: "fluent",
+  chainId: 25363,
+  networkId: 25363,
+  explorers: [
+    {
+        name: "Fluent Explorer",
+        url: "https://fluentscan.xyz",
+        standard: "EIP3091",
+    },
+  ],
+};
