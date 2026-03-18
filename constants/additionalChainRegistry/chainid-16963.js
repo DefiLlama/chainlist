832302088c05f5ed3d529daa8ce08606861d606f
calypsocharm <72401787+calypsocharm@users.noreply.github.com>
2026-03-18 03:16:25 -0600

    Add BotCash Sovereign L2 (#2572)
    * Add BotCash Sovereign L2
    
    * Update chainid-16963.jsUpdate explorer URL to scan.botcash.io
    
    * Update chainid-16963.js
    
    ---------
    
    Co-authored-by: Define101 <93603962+Define101@users.noreply.github.com>

---
+++ constants/additionalChainRegistry/chainid-16963.js
@@ -0,0 +1,25 @@
+export const data = {
+      "name": "BotCash Sovereign L2",
+      "chain": "BOTCY",
+      "rpc": [
+              "https://rpc.botcash.io"
+            ],
+      "faucets": [],
+      "nativeCurrency": {
+              "name": "Bot Cash",
+              "symbol": "BOTCY",
+              "decimals": 18
+      },
+      "features": [],
+      "infoURL": "https://botcash.io",
+      "shortName": "botcy",
+      "chainId": 16963,
+      "networkId": 16963,
+      "icon": "botcash",
+      "explorers": [{
+              "name": "CashScan",
+              "url": "https://scan.botcash.io",
+              "icon": "botcash",
+              "standard": "EIP3091"
+      }]
+}
