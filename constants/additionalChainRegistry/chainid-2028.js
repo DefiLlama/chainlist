413a8bbf92c3bb73812cb728c1a8cdc93866df23
Bekjan <ergeshov@MacBook-Pro-Bekjan.local>
2026-03-25 07:43:49 +0600

    Add ArmaChain Testnet (Chain ID 2028)

---
+++ constants/additionalChainRegistry/chainid-2028.js
@@ -0,0 +1,30 @@
+export const data = {
+  "name": "ArmaChain Testnet",
+  "chain": "ARMA",
+  "icon": "armachain",
+  "rpc": [
+    "https://rpc.armascan.io"
+  ],
+  "features": [{ "name": "EIP155" }],
+  "faucets": [
+    "https://armafaucet.io"
+  ],
+  "nativeCurrency": {
+    "name": "tARMA",
+    "symbol": "tARMA",
+    "decimals": 18
+  },
+  "infoURL": "https://armadex.io",
+  "shortName": "arma-testnet",
+  "chainId": 2028,
+  "networkId": 2028,
+  "slip44": 1,
+  "explorers": [
+    {
+      "name": "ArmaScan",
+      "url": "https://armascan.io",
+      "icon": "armachain",
+      "standard": "EIP3091"
+    }
+  ]
+}
