4832bc5d8cfb4052b64736a2db046c2365873ca0
djsplice <barrows.jeff@gmail.com>
2026-03-25 10:57:27 -0700

    Add ChainIDs 4611 and 4612 for HashSphere

---
+++ constants/additionalChainRegistry/chainid-4611.js
@@ -0,0 +1,17 @@
+{
+    "name": "HashSphere-4611",
+    "chain": "HashSphere",
+    "rpc": [],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "hbar",
+      "symbol": "HBAR",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "infoURL": "https://hashgraph.com",
+    "shortName": "hbar-4611",
+    "chainId": 4611,
+    "networkId": 4611,
+    "explorers": []
+  }
---
+++ constants/additionalChainRegistry/chainid-4612.js
@@ -0,0 +1,17 @@
+{
+    "name": "HashSphere-4612",
+    "chain": "HashSphere",
+    "rpc": [],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "hbar",
+      "symbol": "HBAR",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "infoURL": "https://hashgraph.com",
+    "shortName": "hbar-4612",
+    "chainId": 4612,
+    "networkId": 4612,
+    "explorers": []
+  }
