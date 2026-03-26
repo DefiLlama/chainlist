3452df987565ead3114c7cce0c2300c1556d346b
Báu Trần Duy <33424891+tranduybau@users.noreply.github.com>
2026-03-26 18:56:13 +0700

    feat: add STO Chain mainnet (1306) and testnet (1999)

---
+++ constants/additionalChainRegistry/chainid-1999.js
@@ -0,0 +1,29 @@
+export const data = {
+  "name": "STO Chain Testnet",
+  "chain": "TSTOC",
+  "rpc": [
+    "https://evm-stoc-testnet.stochainscan.io"
+  ],
+  "faucets": [
+    "https://testnet.stochainscan.io/en/request-faucet"
+  ],
+  "nativeCurrency": {
+    "name": "TSTOC",
+    "symbol": "TSTOC",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://www.stochain.io/",
+  "shortName": "tstoc",
+  "chainId": 1999,
+  "networkId": 1999,
+  "icon": "stoc",
+  "explorers": [
+    {
+      "name": "STO Chain Testnet Explorer",
+      "url": "https://testnet.stochainscan.io/en",
+      "standard": "none"
+    }
+  ],
+  "status": "active"
+};
