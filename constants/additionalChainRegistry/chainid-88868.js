dcb17d10647e6ce9598021a090ff70aa3b1f04ed
QTS <lihanhan557@gmail.com>
2026-04-07 07:13:38 +0700

    Add Quantumfluxshield Mainnet configuration

---
+++ constants/additionalChainRegistry/chainid-88868.js
@@ -0,0 +1,30 @@
+{
+  "name": "Quantumfluxshield Mainnet",
+  "chain": "QTS",
+  "icon": "qts",
+  "rpc": [
+    "https://mainnet.qts-chain.com",
+    "https://rpc-1.qts-chain.com",
+    "https://rpc-2.qts-chain.com",
+    "https://rpc-3.qts-chain.com",
+    "https://rpc-4.qts-chain.com",
+    "https://rpc-5.qts-chain.com",
+  ],
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  "nativeCurrency": {
+    "name": "Quantumflux shield",
+    "symbol": "QTS",
+    "decimals": 18
+  },
+  "infoURL": "https://qts-chain.com",
+  "shortName": "qts",
+  "chainId": 88868,
+  "networkId": 88868,
+  "explorers": [
+    {
+      "name": "QTS Explorer",
+      "url": "https://explorer.qts-chain.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
