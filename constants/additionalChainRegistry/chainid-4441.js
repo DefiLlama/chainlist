b5ac6b97d0b89508ea90c7de597f5e564029ea58
Den <dzianis.smalonski@protofire.io>
2026-04-09 16:26:58 +0200

    Add Liteforge Testnet

--- constants/additionalChainRegistry/chainid-4441.js
+++ constants/additionalChainRegistry/chainid-4441.js
@@ -1,24 +1,22 @@
 export const data = {
-    "name": "LitVM Liteforge Testnet",
-    "chain": "Liteforge",
-    "rpc": [
-      "https://liteforge.rpc.caldera.xyz/http"
-    ],
-    "features": [{ "name": "EIP155" }],
-    "faucets": [],
-    "nativeCurrency": {
-      "name": "zkLTC",
-      "symbol": "zkLTC",
-      "decimals": 18
+  name: "LitVM Liteforge Testnet",
+  chain: "Liteforge",
+  rpc: ["https://liteforge.rpc.caldera.xyz/http"],
+  features: [{ name: "EIP155" }],
+  faucets: [],
+  nativeCurrency: {
+    name: "zkLTC",
+    symbol: "zkLTC",
+    decimals: 18,
+  },
+  shortName: "liteforge",
+  chainId: 4441,
+  networkId: 4441,
+  explorers: [
+    {
+      name: "Liteforge Explorer",
+      url: "https://liteforge.explorer.caldera.xyz",
+      standard: "EIP3091",
     },
-    "shortName": "liteforge",
-    "chainId": 4441,
-    "networkId": 4441,
-    "explorers": [
-      {
-        "name": "Liteforge Explorer",
-        "url": "https://liteforge.explorer.caldera.xyz",
-        "standard": "EIP3091"
-      }
-    ]
-  }
+  ],
+};
