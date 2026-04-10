ac2dde918e369a00e447a65aeaae8f30c599fe44
trustburn79-droid <trustburn79@gmail.com>
2026-04-10 19:31:40 +0900

    Add TBURN Mainnet (chainId: 5800)

---
+++ constants/additionalChainRegistry/chainid-5800.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "TBURN Mainnet",
+  "chain": "TBURN",
+  "icon": "tburn",
+  "rpc": [
+    "https://rpc.tburn.io"
+  ],
+  "features": [{ "name": "EIP155" }],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "TBURN",
+    "symbol": "TBURN",
+    "decimals": 18
+  },
+  "infoURL": "https://tburn.io",
+  "shortName": "tburn",
+  "chainId": 5800,
+  "networkId": 5800,
+  "explorers": [
+    {
+      "name": "TBURN Scan",
+      "url": "https://scan.tburn.io",
+      "standard": "EIP3091"
+    }
+  ]
+}
