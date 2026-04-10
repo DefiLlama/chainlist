10ddaf8bd8a1a291e8ecc42a23e4cf0307846aba
trustburn79-droid <trustburn79@gmail.com>
2026-04-10 19:32:37 +0900

    Add TBURN Subnet (chainId: 58001)

---
+++ constants/additionalChainRegistry/chainid-58001.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "TBURN Subnet",
+  "chain": "TBURN",
+  "icon": "tburn",
+  "rpc": [
+    "https://subrpc.tburn.io"
+  ],
+  "features": [{ "name": "EIP155" }],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "TBURN",
+    "symbol": "TBURN",
+    "decimals": 18
+  },
+  "infoURL": "https://tburn.io",
+  "shortName": "tburn-subnet",
+  "chainId": 58001,
+  "networkId": 58001,
+  "explorers": [
+    {
+      "name": "TBURN Subnet Scan",
+      "url": "https://subscan.tburn.io",
+      "standard": "EIP3091"
+    }
+  ]
+}
