117fc32be417654fed61c1717f9d57bbe868fb03
QTS <lihanhan557@gmail.com>
2026-04-14 19:56:42 +0700

    Update chain details for QTSCHAIN Mainnet

--- constants/additionalChainRegistry/chainid-88868.js
+++ constants/additionalChainRegistry/chainid-88868.js
@@ -1,23 +1,20 @@
 {
-  "name": "Quantumfluxshield Mainnet",
+  "name": "QTSCHAIN Mainnet",
   "chain": "QTS",
-  "icon": "qts",
+  "icon": "https://qts-chain.com/icon/qts.png",
   "rpc": [
-    "https://mainnet.qts-chain.com",
     "https://rpc-1.qts-chain.com",
     "https://rpc-2.qts-chain.com",
-    "https://rpc-3.qts-chain.com",
-    "https://rpc-4.qts-chain.com",
-    "https://rpc-5.qts-chain.com",
+    "https://rpc-3.qts-chain.com"
   ],
   features: [{ name: "EIP155" }, { name: "EIP1559" }],
   "nativeCurrency": {
-    "name": "Quantumflux shield",
+    "name": "QTS",
     "symbol": "QTS",
     "decimals": 18
   },
   "infoURL": "https://qts-chain.com",
-  "shortName": "qts",
+  "shortName": "qtschain",
   "chainId": 88868,
   "networkId": 88868,
   "explorers": [
