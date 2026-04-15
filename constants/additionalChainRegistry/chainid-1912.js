532ff464b0a38ae0fab3a7135390dd2f96f29ee5
Besa Chain <besachain.team@gmail.com>
2026-04-15 10:48:04 +0200

    Add BesaChain network configurations (#2638)
    * Add BesaChain network configurations
    
    Add support for BesaChain L1 and L2 networks:
    - BesaChain L1 Mainnet (Chain ID: 1444)
    - BesaChain L2 Mainnet (Chain ID: 1912)
    - BesaChain L1 Testnet (Chain ID: 14440)
    - BesaChain L2 Testnet (Chain ID: 19120)
    
    All networks support EIP155 and EIP1559 features.
    
    * Move RPC to end of array per Chainlist requirements
    
    * Update chainid-1444.js
    
    Corrected error in domain extension
    
    * Update chainid-14440.js
    
    Corrected error in domain extension
    
    * Update chainid-1912.js
    
    Corrected error in domain extension
    
    * Update chainid-19120.js
    
    Corrected error in domain extension
    
    ---------
    
    Co-authored-by: BesaLabs <dev@besachain.io>

---
+++ constants/additionalChainRegistry/chainid-1912.js
@@ -0,0 +1,22 @@
+export const data = {
+  name: "BesaChain L2",
+  chain: "BESA",
+  features: [
+    { name: "EIP155" },
+    { name: "EIP1559" }
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Besa",
+    symbol: "BESA",
+    decimals: 18
+  },
+  infoURL: "https://besachain.com",
+  shortName: "besa-l2",
+  chainId: 1912,
+  networkId: 1912,
+  explorers: [],
+  rpc: [
+    "https://l2-rpc.besachain.com"
+  ]
+};
---
+++ constants/additionalChainRegistry/chainid-19120.js
@@ -0,0 +1,22 @@
+export const data = {
+  name: "BesaChain L2 Testnet",
+  chain: "BESA",
+  features: [
+    { name: "EIP155" },
+    { name: "EIP1559" }
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Test Besa",
+    symbol: "tBESA",
+    decimals: 18
+  },
+  infoURL: "https://besachain.com",
+  shortName: "besa-l2-testnet",
+  chainId: 19120,
+  networkId: 19120,
+  explorers: [],
+  rpc: [
+    "https://testnet-l2-rpc.besachain.com"
+  ]
+};
