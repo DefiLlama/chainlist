94816b2f5ce83e26e806eb2f962fa30cd47c6880
Define101 <Lovelovenow@protonmail.com>
2026-04-17 10:07:11 +0100

    add 88868

---
+++ constants/additionalChainRegistry/chainid-88868.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "QTSCHAIN Mainnet",
+  "chain": "QTS",
+  "icon": "https://qts-chain.com/icon/qts.png",
+  "rpc": [
+    "https://rpc-1.qts-chain.com",
+    "https://rpc-2.qts-chain.com",
+    "https://rpc-3.qts-chain.com"
+  ],
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  "nativeCurrency": {
+    "name": "QTS",
+    "symbol": "QTS",
+    "decimals": 18
+  },
+  "infoURL": "https://qts-chain.com",
+  "shortName": "qtschain",
+  "chainId": 88868,
+  "networkId": 88868,
+  "explorers": [
+    {
+      "name": "QTS Explorer",
+      "url": "https://explorer.qts-chain.com",
+      "icon": "https://qts-chain.com/icon/qts.png",
+      "standard": "EIP3091"
+    }
+  ]
+}
