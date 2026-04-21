135643d8434d5a41c55eea16d26bde44b046f046
AIC Chain <jessonmo19@gmail.com>
2026-04-21 10:42:56 +0800

    Add AICChain Testnet (Chain ID 30181)
    Add support for AICChain Testnet with the following details:
    
    ### Network Information
    - **Network Name**: AICChain Testnet
    - **Chain ID**: 30181
    - **RPC Endpoint**: https://testrpc.aicchain.io
    - **WebSocket**: wss://testws.aicchain.io
    - **Native Currency**: tAIC (18 decimals)
    - **Block Explorer**: https://testscan.aicchain.io
    - **Logo**: https://aic-assets.aicchain.io/Logo_Black.png (500x500)
    
    ### Features Supported
    - EIP155: Chain ID replay protection
    - EIP1559: London hard fork (Dynamic fees)
    
    ### Files Modified
    - Added `constants/additionalChainRegistry/chainid-30181.js`
    
    This PR enables AICChain Testnet to be added to Chainlist, allowing users to:
    1. Access the testnet through chainlist.org
    2. Add the network to MetaMask and other Web3 wallets
    3. Query network information via the Chainlist API
    
    ### Related Links
    - AICChain Official: https://www.aicchain.io
    - Testnet Explorer: https://testscan.aicchain.io

---
+++ constants/additionalChainRegistry/chainid-30181.js
@@ -0,0 +1,45 @@
+export const data = {
+  "name": "AICChain Testnet",
+  "chain": "tAIC",
+  "rpc": [
+    "https://testrpc.aicchain.io"
+  ],
+  "wss": [
+    "wss://testws.aicchain.io"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "AICChain Testnet",
+    "symbol": "tAIC",
+    "decimals": 18
+  },
+  "features": [
+    {
+      "name": "EIP155"
+    },
+    {
+      "name": "EIP1559"
+    }
+  ],
+  "infoURL": "https://www.aicchain.io",
+  "shortName": "taic",
+  "chainId": 30181,
+  "networkId": 30181,
+  "icon": "https://aic-assets.aicchain.io/Logo_Black.png",
+  "icons": [
+    {
+      "url": "https://aic-assets.aicchain.io/Logo_Black.png",
+      "width": 500,
+      "height": 500,
+      "format": "png"
+    }
+  ],
+  "explorers": [
+    {
+      "name": "AICChain Testnet Explorer",
+      "url": "https://testscan.aicchain.io",
+      "icon": "aic",
+      "standard": "EIP3091"
+    }
+  ]
+}
