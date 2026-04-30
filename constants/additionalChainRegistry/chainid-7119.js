4a755185f4aba66e5770f14c261aadb3fde40974
Satya Kwok <satyakwok@users.noreply.github.com>
2026-04-30 14:14:42 +0200

    Add Sentrix Chain (mainnet 7119, testnet 7120)
    Sentrix is an open-source Layer 1 blockchain (Rust + EVM via revm)
    with DPoS+BFT consensus and 1-second blocks.
    
    Mainnet: chainId 7119, RPC https://rpc.sentrixchain.com
    Testnet: chainId 7120, RPC https://testnet-rpc.sentrixchain.com
    
    Both entries point at the canonical Sentrix Scan + Blockscout
    explorers (Blockscout is EIP-3091 conformant; Sentrix Scan is a
    custom Next.js explorer).

---
+++ constants/additionalChainRegistry/chainid-7119.js
@@ -0,0 +1,30 @@
+export const data = {
+    "name": "Sentrix Chain",
+    "chain": "SRX",
+    "rpc": [
+      "https://rpc.sentrixchain.com"
+    ],
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Sentrix",
+      "symbol": "SRX",
+      "decimals": 18
+    },
+    "infoURL": "https://sentrixchain.com",
+    "shortName": "srx",
+    "chainId": 7119,
+    "networkId": 7119,
+    "explorers": [
+      {
+        "name": "Sentrix Scan",
+        "url": "https://scan.sentrixchain.com",
+        "standard": "none"
+      },
+      {
+        "name": "Blockscout",
+        "url": "https://blockscout.sentrixchain.com",
+        "standard": "EIP3091"
+      }
+    ]
+  }
