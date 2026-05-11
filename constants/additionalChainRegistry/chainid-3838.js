a006b8723364de69946142779bac5bf578b961b6
favocode <scyxzh@gmail.com>
2026-05-11 08:45:13 +0000

    feat: add FAVO Mainnet (chainId 3838)

---
+++ constants/additionalChainRegistry/chainid-3838.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "FAVO Mainnet",
+  "chain": "FAVO",
+  "rpc": ["https://rpc.favoscan.com"],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "FAVO",
+    "symbol": "FAVO",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://www.favoscan.com",
+  "shortName": "favo",
+  "chainId": 3838,
+  "networkId": 3838,
+  "icon": "https://www.favoscan.com/token-logos/favo-logo-512.png",
+  "explorers": [{
+    "name": "favoscan",
+    "url": "https://www.favoscan.com",
+    "icon": "https://www.favoscan.com/token-logos/favo-logo-512.png",
+    "standard": "EIP3091"
+  }]
+}
