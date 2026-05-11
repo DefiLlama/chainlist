38ca8d0cf6fca3bdb764fb13b86d0e9e0e81705f
BigtoMantraDev <bigto@mantra.finance>
2026-05-11 18:53:24 +0800

    feat: add NVNM Mainnet EVM configuration

---
+++ constants/additionalChainRegistry/chainid-1611.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "NVNM Mainnet EVM",
+  "chain": "NVNM",
+  "icon": "nvnm",
+  "rpc": [
+    "https://evm.nvnmchain.io",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "MANTRA USD",
+    "symbol": "MANTRAUSD",
+    "decimals": 18
+  },
+  "infoURL": "https://www.nvnmchain.io/",
+  "shortName": "nvnm-evm",
+  "chainId": 1611,
+  "networkId": 1611,
+  "explorers": [
+    {
+      "name": "NVNM Chain Mainnet explorer",
+      "url": "https://explorer.evm.nvnmchain.io/",
+      "standard": "EIP3091",
+      "icon": "nvnm"
+    }
+  ]
+}
