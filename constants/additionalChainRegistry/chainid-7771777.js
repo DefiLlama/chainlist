0b26eec2942d2a222ae9c4559241a1cd3b21f463
VALYGO <248640255+VYO-Labs@users.noreply.github.com>
2026-05-15 13:00:49 +0400

    Add files via upload

---
+++ constants/additionalChainRegistry/chainid-7771777.js
@@ -0,0 +1,27 @@
+export default {
+  "name": "VALYGO Smart Contract",
+  "chain": "VYO",
+  "rpc": [
+    "https://rpc-gw-1.vyoscan.com/ext/bc/2t51dXsuxUvd9teY9TKEJmgxmxMk3CRF88UYTA4HQgjeYZqzSX/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "VYO",
+    "symbol": "VYO",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://vyochain.com",
+  "shortName": "vyosc",
+  "chainId": 7771777,
+  "networkId": 7771777,
+  "icon": "valygo",
+  "explorers": [
+    {
+      "name": "VYOScan",
+      "url": "https://vyoscan.com",
+      "icon": "valygo",
+      "standard": "EIP3091"
+    }
+  ]
+}
