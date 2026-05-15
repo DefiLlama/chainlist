10d6eeed08d2ee05b17d54e5080c38597da61055
lemengbin <lemengbin@163.com>
2026-05-15 19:25:59 +0800

    Add Safe(AnWang) Mainnet/Testnet

---
+++ constants/additionalChainRegistry/chainid-6666665.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "Safe(AnWang) Mainnet",
+  "chain": "Safe(AnWang)",
+  "rpc": [
+    "https://safe4.anwang.com/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "SAFE(AnWang)",
+    "symbol": "SAFE",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" }
+  ],
+  "infoURL": "https://www.anwang.com",
+  "shortName": "SafeMainnet",
+  "chainId": 6666665,
+  "networkId": 6666665,
+  "explorers": [
+    {
+      "name": "Safe(AnWang) Explorer",
+      "url": "http://safe4.anwang.com",
+      "standard": "EIP3091"
+    }
+  ],
+  "isTestnet": false
+}
---
+++ constants/additionalChainRegistry/chainid-6666666.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "Safe(AnWang) Testnet",
+  "chain": "Safe(AnWang)",
+  "rpc": [
+    "https://safe4testnet.anwang.com/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "SAFE(AnWang)",
+    "symbol": "SAFE",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" }
+  ],
+  "infoURL": "https://www.anwang.com",
+  "shortName": "SafeTestnet",
+  "chainId": 6666666,
+  "networkId": 6666666,
+  "explorers": [
+    {
+      "name": "Safe(AnWang) Testnet Explorer",
+      "url": "http://safe4testnet.anwang.com",
+      "standard": "EIP3091"
+    }
+  ],
+  "isTestnet": true
+}
