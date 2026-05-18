8e71ab279302c9338d32e1099de222b8ce3dfaec
King <69189392+King11919@users.noreply.github.com>
2026-05-19 03:24:50 +0530

    Add files via upload

---
+++ chainid-484.js
@@ -0,0 +1,24 @@
+export const data = {
+  "chainId": 484,
+  "name": "BlockSurety",
+  "shortName": "BSURE",
+  "chain": "BSURE",
+  "networkId": 484,
+  "infoURL": "https://main.blocksurety.net:484",
+  "rpc": ["https://main.blocksurety.net:484/"],
+  "nativeCurrency": {
+    "name": "BSURE",
+    "symbol": "BSURE",
+    "decimals": 18
+  },
+  "explorers": [
+    {
+      "name": "BlockSurety Explorer",
+      "url": "https://main.blocksurety.net:484",
+      "standard": "EIP3091"
+    }
+  ],
+  "faucets": [],
+  "features": ["EVM Compatible", "MetaMask Supported"],
+  "icon": " "
+};
