e9192ab96088f88ca372e573895b10e89f7b4c37
Equious <equious@gmail.com>
2026-05-20 11:12:10 -0600

    Add BattleChain Mainnet (chainId 626)
    ZKsync-based Ethereum L2. Bridged ETH for gas.

---
+++ constants/additionalChainRegistry/chainid-626.js
@@ -0,0 +1,26 @@
+export const data = {
+    "name": "BattleChain Mainnet",
+    "chain": "BattleChain",
+    "rpc": [
+      "https://mainnet.battlechain.com"
+    ],
+    "features": [{ "name": "EIP155" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Ether",
+      "symbol": "ETH",
+      "decimals": 18
+    },
+    "infoURL": "https://battlechain.com",
+    "shortName": "battlechain",
+    "chainId": 626,
+    "networkId": 626,
+    "status": "active",
+    "explorers": [
+      {
+        "name": "BattleChain Explorer",
+        "url": "https://explorer.battlechain.com",
+        "standard": "EIP3091"
+      }
+    ]
+  };
