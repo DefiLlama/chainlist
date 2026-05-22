76f7349afa5485c9cb3570a41afc00d51f04c08f
Ajvlogger <ajvlogger4@gmail.com>
2026-05-23 05:24:53 +0800

    Update chainid-2757.js

--- constants/additionalChainRegistry/chainid-2757.js
+++ constants/additionalChainRegistry/chainid-2757.js
@@ -1,19 +1,32 @@
 {
-  "name": "Medorcoin Mainnet",
+  "name": {
+    "name": "Medorcoin Mainnet",
+    "history": [
+      {
+        "url": "https://medorcoin.org",
+        "name": "Medorcoin Mainnet"
+      }
+    ]
+  },
   "chain": "MEDOR",
-  "rpc": ["http://172.235.50.31:3000"],
+  "rpc": [
+    "https://rpc.medorcoin.org"
+  ],
+  "faucets": [],
   "nativeCurrency": {
     "name": "Medorcoin",
-    "symbol": "MEDOR",
+    "symbol": "MDC",
     "decimals": 18
   },
   "infoURL": "https://medorcoin.org",
   "shortName": "medor",
   "chainId": 2757,
   "networkId": 2757,
-  "explorers": [{
-    "name": "MedorExplorer",
-    "url": "https://medorcoin.org",
-    "standard": "EIP3091"
-  }]
+  "explorers": [
+    {
+      "name": "MedorExplorer",
+      "url": "https://medorcoin.org",
+      "standard": "EIP3091"
+    }
+  ]
 }
