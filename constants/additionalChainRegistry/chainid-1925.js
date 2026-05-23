fc2d0b02bc981656720b6f3494923e30f9f34a27
Tipspay Architect <developer@tipspay.org>
2026-05-23 07:55:30 +0300

    Create chainid-1925.js

---
+++ constants/additionalChainRegistry/chainid-1925.js
@@ -0,0 +1,26 @@
+{
+  "name": "TipsChain",
+  "chain": "TPC",
+  "rpc": [
+    "https://rpc.tipschain.org"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Tips Coin",
+    "symbol": "TPC",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://tipschain.org",
+  "shortName": "tpc",
+  "chainId": 1925,
+  "networkId": 1925,
+  "icon": "tipschain",
+  "explorers": [
+    {
+      "name": "TipsChain Explorer",
+      "url": "https://tipschain.online",
+      "standard": "EIP3091"
+    }
+  ]
+}
