d08a0d4569163813af9fcb475614dcb69ec2db2e
ron-elmekiesse-bloxroute <ron.elmekiesse@bloxroute.com>
2026-05-28 11:08:40 +0300

    Remove bloxroute x layer endpoints

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -4677,16 +4677,6 @@ export const extraRpcs = {
         trackingDetails: privacyStatement.bloxroute,
       },
       {
-        url: "https://okx-xlayer.rpc.blxrbdn.com",
-        tracking: "yes",
-        trackingDetails: privacyStatement.bloxroute,
-      },
-      {
-        url: "https://flap-xlayer.rpc.blxrbdn.com",
-        tracking: "yes",
-        trackingDetails: privacyStatement.bloxroute,
-      },
-      {
         url: "https://xlayer-mainnet.rpc.sentio.xyz",
         tracking: "limited",
         trackingDetails: privacyStatement.sentio,
