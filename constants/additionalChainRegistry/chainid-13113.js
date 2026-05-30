03a98909df749a2177a11eb9200818cf184cf267
Ilia GH. (Symbolic Architect – RANNTA) <ilia144000@gmail.com>
2026-05-30 16:02:38 +0400

    Add RANNTA X-Chain configuration

---
+++ constants/additionalChainRegistry/chainid-13113.js
@@ -0,0 +1,24 @@
+export const data = {
+  name: "RANNTA X-Chain",
+  chain: "RANNTA",
+  rpc: ["https://rpc.rannta.com/"],
+  nativeCurrency: {
+    name: "RANNTA Core X",
+    symbol: "RNTX",
+    decimals: 18,
+  },
+  features: [],
+  infoURL: "https://rannta.com/",
+  shortName: "rntx",
+  chainId: 13113,
+  networkId: 13113,
+  icon: "https://ranntaexchange.com/brand/rannta-xchain-256.png",
+  explorers: [
+    {
+      name: "RANNTA X-Chain Explorer",
+      url: "https://explorer.rannta.com/",
+      icon: "https://ranntaexchange.com/brand/rannta-xchain-256.png",
+      standard: "EIP3091",
+    },
+  ],
+};
