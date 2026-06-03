17480201414c12ba180fb16204487ccefd4f20da
Astroashutosh <ashutosh.yadav4096@gmail.com>
2026-06-03 12:36:40 +0530

    Add icon for NirmalX Smart Chain

--- constants/additionalChainRegistry/chainid-15526.js
+++ constants/additionalChainRegistry/chainid-15526.js
@@ -1,23 +1,36 @@
-export default {
+export const data = {
   name: "NirmalX Smart Chain",
   chain: "NRXN",
+
   rpc: [
     "https://rpc.nirmalxscan.com"
   ],
+
   faucets: [],
+
   nativeCurrency: {
     name: "NirmalX Smart Chain",
     symbol: "NRXN",
     decimals: 18
   },
-  infoURL: "https://nirmalxscan.com",
+
+ features: [
+    { name: "EIP155" },
+    { name: "EIP1559" }
+  ],
+
+  infoURL: "https://nirmalx.io/",
   shortName: "nrxn",
   chainId: 15526,
   networkId: 15526,
+
+  icon: "https://gateway.pinata.cloud/ipfs/bafybeib2n3272ga73alqnis6lsvlnjvt5hilihsf4yfxo4t7ebs6exejhm",
+
   explorers: [
     {
       name: "NirmalX Explorer",
       url: "https://nirmalxscan.com",
+      icon: "https://gateway.pinata.cloud/ipfs/bafybeib2n3272ga73alqnis6lsvlnjvt5hilihsf4yfxo4t7ebs6exejhm",
       standard: "EIP3091"
     }
   ]
