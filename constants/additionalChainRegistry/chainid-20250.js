0b2137391ff3914cc62b60c3d269b36e4a7da3ed
cryptoexdevcheese <cryptoexdevcheese@gmail.com>
2026-06-18 17:51:14 +0800

    Add configuration for CHEESE Blockchain (#2815)
    * Add configuration for CHEESE Blockchain
    
    * Export data from chainid-20250.js
    
    ---------
    
    Co-authored-by: Define101 <93603962+Define101@users.noreply.github.com>

---
+++ constants/additionalChainRegistry/chainid-20250.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "CHEESE Blockchain",
+  "chain": "CHEESE",
+  "rpc": [
+    "https://cheeseblockchain.com/api/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "NCheese",
+    "symbol": "NCH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://cheeseblockchain.com",
+  "shortName": "cheese",
+  "chainId": 20250,
+  "networkId": 20250,
+  "icon": "https://cheeseblockchain.com/wallet-logos/cheese-blockchain-256.png",
+  "explorers": [{
+    "name": "CHEESE Explorer",
+    "url": "https://cheeseblockchain.com/explorer",
+    "icon": "https://cheeseblockchain.com/wallet-logos/cheese-blockchain-128.png",
+    "standard": "EIP3091"
+  }]
+}
