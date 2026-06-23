1f611d06114b674180f5c2a5e9b91998d5f0dc26
YudaAdiPratama <github.yuda@gmail.com>
2026-06-24 00:21:33 +0200

    Add Vexanium EVM (chainId 6736)

---
+++ constants/additionalChainRegistry/chainid-6736.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "Vexanium EVM",
+    "chain": "VEX",
+    "rpc": [
+      "https://vexascan.com/rpc"
+    ],
+    "features": [{ "name": "EIP155" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "Vexanium",
+      "symbol": "VEX",
+      "decimals": 18
+    },
+    "infoURL": "https://vexascan.com",
+    "shortName": "vex",
+    "chainId": 6736,
+    "networkId": 6736,
+    "explorers": [
+      {
+        "name": "Vexascan",
+        "url": "https://vexascan.com",
+        "standard": "EIP3091"
+      }
+    ]
+  }
