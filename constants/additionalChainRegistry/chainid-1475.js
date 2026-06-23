20eb1e5e166275c6b35a7075bf19da18ae9d9381
Cliffyclif <81383762+Cliffyclif@users.noreply.github.com>
2026-06-23 09:43:29 +0200

    Add CRYMAD Chain Mainnet Network (Chain ID 1475)

---
+++ constants/additionalChainRegistry/chainid-1475.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "CRYMAD Chain Mainnet Network",
+  "chain": "CMX",
+  "icon": "crymadchain",
+  "rpc": [
+    "https://rpc.cmxofficial.com"
+  ],
+  "faucets": [],
+  "features": [
+    { "name": "EIP155" }
+  ],
+  "nativeCurrency": {
+    "name": "CRYMADX REAL",
+    "symbol": "CMX-R",
+    "decimals": 18
+  },
+  "infoURL": "https://cmxofficial.com/",
+  "shortName": "CMX-R",
+  "chainId": 1475,
+  "networkId": 1,
+  "explorers": [
+    {
+      "name": "crymadchain explorers",
+      "url": "https://explorer.cmxofficial.com/"
+    }
+  ],
+  "status": "active"
+}
