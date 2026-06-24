14fff6350d466650d18011d1d35665f293c5b9d7
Shivam Maurya <shivam@Shivams-MacBook-Air.local>
2026-06-24 15:51:45 +0530

    add new chain i.e, chainid-262144

---
+++ constants/additionalChainRegistry/chainid-262144.js
@@ -0,0 +1,27 @@
+  export const data = {
+    "name": "NOWA Devnet",
+    "title": "NOWA Devnet",
+    "chain": "NOWA",
+    "icon": "nowa",
+    "rpc": [
+      "https://node1.nowa.finance",
+       "https://node2.nowa.finance",
+    ],
+    "faucets": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "nativeCurrency": {
+      "name": "NOWA",
+      "symbol": "NOWA",
+      "decimals": 18
+    },
+    "infoURL": "https://nowa.finance/",
+    "shortName": "nowachain",
+    "chainId": 262144,
+    "networkId": 262144,
+    "explorers": [
+      {
+        "name": "Nowa Explorer",
+        "url": "https://explorer.nowa.finance",
+        "standard": "EIP3091"
+      }
+    ]
+  }
