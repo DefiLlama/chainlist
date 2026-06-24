6948fde0579d5c173655d5716150a323fd12b1c5
nicewednesss-ux <nicewednesss@gmail.com>
2026-06-25 00:20:33 +0300

    Create chainid-588588.js

---
+++ constants/additionalChainRegistry/chainid-588588.js
@@ -0,0 +1,32 @@
+{
+  "name": "Ethereum2",
+  "chain": "ETH2",
+  "rpc": [
+    "https://rpc.ethereum-2.org/"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ethereum2",
+    "symbol": "ETH2",
+    "decimals": 18
+  },
+  "features": [
+    {
+      "name": "EIP155"
+    },
+    {
+      "name": "EIP1559"
+    }
+  ],
+  "infoURL": "https://ethereum-2.org/",
+  "shortName": "eth2",
+  "chainId": 588588,
+  "networkId": 588588,
+  "explorers": [
+    {
+      "name": "Ethereum2 Explorer",
+      "url": "https://explorer.ethereum-2.org/",
+      "standard": "EIP3091"
+    }
+  ]
+}
