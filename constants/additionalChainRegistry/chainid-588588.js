1adcb0ec7b115841740250942b3e0820636b73fe
nicewednesss-ux <nicewednesss@gmail.com>
2026-06-26 15:20:43 +0300

    Update chainid-588588.js

--- constants/additionalChainRegistry/chainid-588588.js
+++ constants/additionalChainRegistry/chainid-588588.js
@@ -1,13 +1,13 @@
 {
-  "name": "Ethereum2",
-  "chain": "ETH2",
+  "name": "Ethereum1",
+  "chain": "ETH1",
   "rpc": [
-    "https://rpc.ethereum-2.org/"
+    "https://rpc.ethereum1.org/"
   ],
   "faucets": [],
   "nativeCurrency": {
-    "name": "Ethereum2",
-    "symbol": "ETH2",
+    "name": "Ethereum I",
+    "symbol": "ETH1",
     "decimals": 18
   },
   "features": [
@@ -18,14 +18,14 @@
       "name": "EIP1559"
     }
   ],
-  "infoURL": "https://ethereum-2.org/",
-  "shortName": "eth2",
+  "infoURL": "https://ethereum1.org/",
+  "shortName": "eth1",
   "chainId": 588588,
   "networkId": 588588,
   "explorers": [
     {
-      "name": "Ethereum2 Explorer",
-      "url": "https://explorer.ethereum-2.org/",
+      "name": "Ethereum I Explorer",
+      "url": "https://explorer.ethereum1.org/",
       "standard": "EIP3091"
     }
   ]
