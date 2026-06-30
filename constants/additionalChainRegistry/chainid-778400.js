4021834e0c289a0d08e88de6f6660a88f3797ce1
FlxgChain <felixgreenai@gmail.com>
2026-06-30 16:11:53 +0530

    Add Felix Smart Chain configuration

---
+++ constants/additionalChainRegistry/chainid-778400.js
@@ -0,0 +1,25 @@
+export default {
+  name: "Felix Smart Chain",
+  chain: "FLXG",
+  icon: "https://plum-geographical-dinosaur-799.mypinata.cloud/ipfs/bafybeiaq6gzygs2hx2xh2rl4nbeaegcnww65xcmjcevdxdols56jsyskeu",
+  rpc: [
+    "https://rpc.felixexplorer.com"
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "FLXG",
+    symbol: "FLXG",
+    decimals: 18
+  },
+  infoURL: "https://felixexplorer.com",
+  shortName: "flxg",
+  chainId: 778400,
+  networkId: 778400,
+  explorers: [
+    {
+      name: "Felix Smart Chain Explorer",
+      url: "https://felixexplorer.com",
+      standard: "EIP3091"
+    }
+  ]
+};
