c42da2c7fcb0ed501f986688e751427f0fb04f7a
plato9966 <plato9966@proton.me>
2026-07-02 20:02:52 +0800

    Add files via upload

---
+++ constants/additionalChainRegistry/chainid-53.js
@@ -0,0 +1,22 @@
+export const data = {
+  "name": "Plato Mainnet",
+  "chain": "Plato",
+  "rpc": [
+    "https://api-mainnet.plato.exchange",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Plato USDC",
+    "symbol": "USDC",
+    "decimals": 6
+  },
+  "shortName": "plato",
+  "chainId": 53,
+  "networkId": 53,
+  "explorers": [
+    {
+      "name": "Plato Mainnet Explorer",
+      "url": "https://mainnet.plato.exchange/explorer"
+    }
+  ]
+}
