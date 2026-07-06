ec6a587c27c39c5dcf8e147fcb2d49832113a7f9
Ferhan Rana <55444756+ferhanrana@users.noreply.github.com>
2026-07-06 18:21:29 +0800

    feat: add SWSS Chain (chainId 122605) (#2899)

---
+++ constants/additionalChainRegistry/chainid-122605.js
@@ -0,0 +1,28 @@
+export const data = {
+  "name": "SWSS Chain",
+  "chain": "SWSS",
+  "icon": "swss",
+  "rpc": [
+    "https://swssrpc.swss.io"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "SWSS Coin",
+    "symbol": "SWSS",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://swss.io",
+  "shortName": "swss",
+  "chainId": 122605,
+  "networkId": 122605,
+  "explorers": [
+    {
+      "name": "SWSS Chain Scan",
+      "url": "https://swssscan.swss.io",
+      "icon": "swss",
+      "standard": "EIP3091"
+    }
+  ]
+}
+
