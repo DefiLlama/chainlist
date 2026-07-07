b276984e361506a50d939ff71d7ee188da38eb36
unknown <megha.shukla@zeeve.io>
2026-07-07 14:34:44 +0530

    Add OMAChain to ChainList

---
+++ constants/additionalChainRegistry/chainid-6623.js
@@ -0,0 +1,26 @@
+export const data = {
+  "name": "OMAChain Mainnet",
+  "chain": "OMAChain",
+  "icon": "https://f005.backblazeb2.com/file/tracehawk-prod/logo/OMA3/OMA3_Logo.png",
+  "rpc": [
+    "https://rpc.omachain.org/"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "OMA",
+    "symbol": "OMA",
+    "decimals": 18
+  },
+  "infoURL": "",
+  "shortName": "OMA mainnet",
+  "chainId": 6623,
+  "networkId": 6623,
+  "explorers": [
+    {
+      "name": "OMAChain Explorer",
+      "url": "https://explorer.omachain.org",
+      "icon": "https://f005.backblazeb2.com/file/tracehawk-prod/logo/OMA3/OMA3_Logo.png",
+      "standard": "EIP3091"
+    }
+      ]
+}
