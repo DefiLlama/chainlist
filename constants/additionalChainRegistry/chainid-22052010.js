ad15e21e6f200b0ab93b64f36e9ce5dbaa2fd51c
aakash4dev <aakash4dev.crypto@gmail.com>
2026-07-08 16:42:49 +0530

    Added Nowa Testnet

---
+++ constants/additionalChainRegistry/chainid-22052010.js
@@ -0,0 +1,20 @@
+export const data = {
+  name: "Nowa Testnet",
+  chain: "NOWA",
+  rpc: [
+    "https://tnode1.nowa.finance",
+    "https://tnode2.nowa.finance",
+    "https://tnode3.nowa.finance"
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Nowa",
+    symbol: "NOWA",
+    decimals: 18
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://nowa.finance",
+  shortName: "nowa-testnet",
+  chainId: 22052010,
+  networkId: 22052010
+};
