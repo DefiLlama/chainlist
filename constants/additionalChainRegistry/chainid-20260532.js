a220592ca06100b2cf554167ea61cdc5181ae20f
andystorrs <andystorrs@hotmail.com>
2026-07-13 11:18:00 +0100

    Add GridMind Testnet (#2906)

---
+++ constants/additionalChainRegistry/chainid-20260532.js
@@ -0,0 +1,29 @@
+export const data = {
+  name: "GridMind Testnet",
+  title: "GridMind Testnet",
+  chain: "GRD",
+  rpc: ["https://testnet-rpc.grid-mind.com"],
+  faucets: ["https://testnet-faucet.grid-mind.com"],
+  nativeCurrency: {
+    name: "GridMind Test GRD",
+    symbol: "testGRD",
+    decimals: 18,
+  },
+  infoURL: "https://www.grid-mind.com/blockchain",
+  shortName: "gridmindt",
+  chainId: 20260532,
+  networkId: 20260532,
+  explorers: [
+    {
+      name: "GridMind Testnet Explorer",
+      url: "https://gridmindblockchain.com",
+      standard: "EIP3091",
+    },
+    {
+      name: "GridMind Technical Explorer",
+      url: "https://testnet-explorer.grid-mind.com",
+      standard: "EIP3091",
+    },
+  ],
+  testnet: true,
+};
