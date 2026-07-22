8a22dc1f0c4fbabebf6d98e19d60b3fcc80bd4c8
quincynexa <quincynexa@gmail.com>
2026-07-22 17:40:49 +0800

    feat(chain): add Ticko Testnet

---
+++ constants/additionalChainRegistry/chainid-84257.js
@@ -0,0 +1,25 @@
+export const data = {
+  "name": "Ticko Testnet",
+  "chain": "Ticko",
+  "rpc": [
+    "https://miner.testnet.ticko.xyz:4556"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ether",
+    "symbol": "ETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://testnet.ticko.xyz/",
+  "shortName": "ticko-testnet",
+  "chainId": 84257,
+  "networkId": 84257,
+  "icon": "ticko",
+  "explorers": [{
+    "name": "ticko explorer",
+    "url": "https://testnet.ticko.xyz/explorer",
+    "icon": "ticko",
+    "standard": "EIP3091"
+  }]
+}
