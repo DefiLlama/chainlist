f94f368417db8cc6fa5d64ac0529cbdc808f6a30
Utkarsh Sharma <utkarsh630623@gmail.com>
2026-07-23 15:18:36 +0530

    Add Aether Mainnet

--- constants/additionalChainRegistry/chainid-2107.js
+++ constants/additionalChainRegistry/chainid-2107.js
@@ -1,26 +1,25 @@
-export const data = {
-    "name": "IBVM Testnet",
-    "chain": "IBVM Testnet",
-    "icon": "ibvmtest",
-    "rpc": [
-      "https://rpc-testnet.ibvm.io/"
-    ],
-    "faucets": ["https://faucet.ibvm.io"],
-    "nativeCurrency": {
-      "name": "IBVM Bitcoin",
-      "symbol": "IBVM",
-      "decimals": 18
-    },
-    "infoURL": "https://ibvm.io/",
-    "shortName": "IBVMT",
-    "chainId": 2107,
-    "networkId": 2107,
-    "explorers": [
-      {
-        "name": "IBVM Testnet explorer",
-        "url": "https://testnet.ibvmscan.io",
-        "standard": "EIP3091"
-      }
-    ],
-    "status": "active"
-  }
+export default {
+  "name": "Aether",
+  "chain": "AETHER",
+  "rpc": [
+    "https://aether.signaturesoftware.co.in/rpc/"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Aether",
+    "symbol": "AETH",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://aether.signaturesoftware.co.in",
+  "shortName": "aeth",
+  "chainId": 2107,
+  "networkId": 2107,
+  "icon": "aether",
+  "explorers": [{
+    "name": "Aether Explorer",
+    "url": "https://aether.signaturesoftware.co.in",
+    "icon": "aether",
+    "standard": "EIP3091"
+  }]
+}
