69593a5e78af9f942d9c40540349af0cfae56c4a
GTBS Chain <gtbschain@gmail.com>
2026-07-25 15:41:11 +0400

    Create chainid-4599.js

---
+++ constants/additionalChainRegistry/chainid-4599.js
@@ -0,0 +1,25 @@
+{
+  "name": "GTBS Chain",
+  "chain": "GTBS",
+  "rpc": [
+    "https://mainnet-rpc.gtbsblockchain.com",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "GTBS",
+    "symbol": "GTBS",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://gtbs.live",
+  "shortName": "gtbs",
+  "chainId": 4599,
+  "networkId": 4599,
+  "icon": "gtbs",
+  "explorers": [{
+    "name": "gtbs",
+    "url": "https://gtbsblockchain.com",
+    "icon": "gtbs",
+    "standard": "EIP3091"
+  }]
+}
