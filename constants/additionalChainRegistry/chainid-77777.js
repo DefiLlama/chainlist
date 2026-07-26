89abc647b1d4d2af0d8bd6cb4797c960b282a356
aliuzum1 <102461916+aliuzum1@users.noreply.github.com>
2026-07-26 13:47:42 +0300

    Create chainid-77777.js

---
+++ constants/additionalChainRegistry/chainid-77777.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "askol",
+  chain: "ETH",
+  rpc: [
+    "https://kripto-a-.onrender.com"
+  ],
+  faucets: [],
+  nativeCurrency: {
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18
+  },
+  chainId: 77777,
+  networkId: 77777,
+  infoURL: "https://kripto-a-.onrender.com",
+  shortName: "askol",
+  slip44: 60,
+  explorers: []
+};
