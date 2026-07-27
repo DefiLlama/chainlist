dcf8a30774367102d3a00e247028afb92c6acdb2
Julian Wilson <2652683+mrwillis@users.noreply.github.com>
2026-07-27 08:44:06 -0400

    Add SX Network (#2971)

---
+++ constants/additionalChainRegistry/chainid-4162.js
@@ -0,0 +1,23 @@
+export const data = {   
+    "name": "SX Network",
+    "chain": "SXR",
+    "rpc": [
+      "https://rpc-rollup.sx.technology",
+    ],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "SX Token",
+      "symbol": "SX",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+    "infoURL": "https://sx.technology",
+    "shortName": "sx-mainnet",
+    "chainId": 4162,
+    "networkId": 4162,
+    "explorers": [{
+      "name": "SX Explorer",
+      "url": "https://explorer.sx.technology",
+      "standard": "EIP3091"
+    }]
+  }
