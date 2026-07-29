5b851730715069f67963e9c3ec504bf8942a9d4f
Ilia GH. (Symbolic Architect – RANNTA) <ilia144000@gmail.com>
2026-07-29 17:25:15 +0400

    Update RANNTA X-Chain icon to pinned IPFS asset

--- constants/additionalChainRegistry/chainid-13113.js
+++ constants/additionalChainRegistry/chainid-13113.js
@@ -12,12 +12,12 @@ export const data = {
   shortName: "rntx",
   chainId: 13113,
   networkId: 13113,
-  icon: "https://ranntaexchange.com/brand/rannta-xchain-256.png",
+  icon: "https://emerald-important-bat-644.mypinata.cloud/ipfs/bafkreidh2qtmdplg7wi2le7ump57gvfhoyrg565m4tgua3d4b6zgmojqwm",
   explorers: [
     {
       name: "RANNTA X-Chain Explorer",
       url: "https://explorer.rannta.com/",
-      icon: "https://ranntaexchange.com/brand/rannta-xchain-256.png",
+      icon: "https://emerald-important-bat-644.mypinata.cloud/ipfs/bafkreidh2qtmdplg7wi2le7ump57gvfhoyrg565m4tgua3d4b6zgmojqwm",
       standard: "EIP3091",
     },
   ],
