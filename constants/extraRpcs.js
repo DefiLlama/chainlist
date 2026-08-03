74fddabc3bc933dcc11335cac4aa43284518d5f5
Define101 <93603962+Define101@users.noreply.github.com>
2026-08-03 11:21:31 +0100

    fix (#3002)

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -204,7 +204,7 @@
     "We do not collect or store personal request data or request origins. To ensure the functionality of our services, such as load balancing and DDoS protection",
   PulseChainRpc:
     "We do not store or track any user data other than the data publicly available on-chain.https://rpc.pulsechainrpc.com/privacy",
-  mijkpro: = "We log IP addresses and request metadata for rate-limiting and abuse-prevention purposes only. No data is sold, shared with third parties. This data is only used for dashboard analytics.",
+  mijkpro: "We log IP addresses and request metadata for rate-limiting and abuse-prevention purposes only. No data is sold, shared with third parties. This data is only used for dashboard analytics.",
   MBF: "MBF does not use user accounts and does not intentionally collect personally identifying information. When you access our RPC endpoints, the only data we may process are the requesting IP address and the requested method name. We use this limited data solely for operating the service—for example, rate limiting, abuse and DDoS mitigation, debugging, uptime monitoring, and reliability analytics.",
   DHF: "DHF does not use user accounts and does not intentionally collect personally identifying information. When you access our RPC endpoints, the only data we may process are the requesting IP address and the requested method name. We use this limited data solely for operating the service—for example, rate limiting, abuse and DDoS mitigation, debugging, uptime monitoring, and reliability analytics.",
   Stakely:
