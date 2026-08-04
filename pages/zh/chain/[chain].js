8dc34734c22592e93b1b8af1bab0714eabed4601
Ilia GH. (Symbolic Architect – RANNTA) <ilia144000@gmail.com>
2026-08-04 17:25:28 +0400

    Use external icon fallback on Chinese chain page

--- pages/zh/chain/[chain].js
+++ pages/zh/chain/[chain].js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Head from "next/head";
 import Link from "next/link";
 // import { useTranslations } from "next-intl";
-import { notTranslation as useTranslations } from "../../../utils";
+import { getChainIcon, notTranslation as useTranslations } from "../../../utils";
 import { populateChain, fetchWithCache } from "../../../utils/fetch";
 import AddNetwork from "../../../components/chain";
 import Layout from "../../../components/Layout";
@@ -71,9 +71,7 @@ export async function getStaticPaths() {
 function Chain({ chain }) {
   const t = useTranslations("Common", "zh");
 
-  const icon = React.useMemo(() => {
-    return chain?.chainSlug ? `https://icons.llamao.fi/icons/chains/rsz_${chain.chainSlug}.jpg` : "/unknown-logo.png";
-  }, [chain]);
+  const icon = React.useMemo(() => getChainIcon(chain), [chain]);
 
   return (
     <>
