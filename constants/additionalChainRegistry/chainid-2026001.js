071057702f1480e1a96c4dfd2ff39207b40389d7
depjandoxpot-eng <depjandoxpot@gmail.com>
2026-08-05 16:46:56 +0545

    Create chainid-2026001.js

---
+++ constants/additionalChainRegistry/chainid-2026001.js
@@ -0,0 +1,30 @@
+{
+  "name": "Duckpot Testnet",
+  "chain": "DUCK",
+  "rpc": [
+    "https://rpc.duckpot.xyz"
+  ],
+  "faucets": [
+    "https://duckpot-nexus.lovable.app/faucet"
+  ],
+  "nativeCurrency": {
+    "name": "Duckpot",
+    "symbol": "DUCK",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" }
+  ],
+  "infoURL": "https://duckpot-nexus.lovable.app",
+  "shortName": "duck",
+  "chainId": 2026001,
+  "networkId": 2026001,
+  "icon": "duckpot",
+  "explorers": [
+    {
+      "name": "Duckpot Explorer",
+      "url": "https://duckpot-nexus.lovable.app/explorer",
+      "standard": "none"
+    }
+  ]
+}
