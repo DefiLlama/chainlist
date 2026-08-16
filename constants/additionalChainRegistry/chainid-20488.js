8c078ac853b10a97d4759a13973b2522c04af890
Wallet <efikcoin@gmail.com>
2026-08-16 11:34:41 +0200

    Create chainid-20488.js

---
+++ constants/additionalChainRegistry/chainid-20488.js
@@ -0,0 +1,23 @@
+// Chain ID 20488 - Efikcoin Mainnet
+export default {
+  chainId: 20488,
+  chain: "EFC",
+  name: "Efikcoin Mainnet",
+  shortName: "efikcoin",
+  networkId: 20488,
+  nativeCurrency: {
+    name: "Efikcoin",
+    symbol: "EFC",
+    decimals: 18
+  },
+  rpc: ["https://rpc.efikcoin.com/rpc"],
+  faucets: [],
+  explorers: [{
+    name: "Efikcoin Explorer",
+    url: "https://explorer.efikcoin.com",
+    standard: "EIP3091"
+  }],
+  infoURL: "https://efikcoin.com",
+  sourceToken: "0x677Ce9CBa67f7484ea951a12897CE780cFd8fED1",
+  founder: "0xC5AD5cfcF81AD63a94227334b898eafCe6B27cCA"
+}
