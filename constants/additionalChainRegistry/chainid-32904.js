eabd692f38f8834070f419632c7be2b557891433
Shomari <nyusternie@sdot.io>
2026-08-17 06:41:32 -0400

    Add Snowside Mainnet (chainId 32904) and Testnet (chainId 33160) (#3041)
    Snowside is a dedicated Avalanche EVM L1 for eCash.com featuring:
    - Native BTC gas (ECX token)
    - Blind merged mining
    - USDC bridging via ICM (Interchain Messaging)
    
    RPC Endpoints:
    - Mainnet: https://rpc.snowside.network/mainnet
    - Testnet: https://rpc.snowside.network/testnet
    
    Native Currency: ECash (ECX), 18 decimals
    Website: https://snowside.network
    GitHub: https://github.com/abitsuite/snowside

---
+++ constants/additionalChainRegistry/chainid-32904.js
@@ -0,0 +1,23 @@
+export const data = {
+  "name": "Snowside",
+  "chain": "ECX",
+  "rpc": ["https://rpc.snowside.network/mainnet"],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "ECash",
+    "symbol": "ECX",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://snowside.network",
+  "shortName": "ecx",
+  "chainId": 32904,
+  "networkId": 32904,
+  "icon": "ipfs://bafkreig3ioaoqiyvd3uinf5p5qtvvhhzxbbvb3ukljezanccsrc5mfp62y",
+  "explorers": [{
+    "name": "Snowside Explorer",
+    "url": "https://explorer.snowside.network",
+    "icon": "ipfs://bafkreig3ioaoqiyvd3uinf5p5qtvvhhzxbbvb3ukljezanccsrc5mfp62y",
+    "standard": "EIP3091"
+  }]
+}
