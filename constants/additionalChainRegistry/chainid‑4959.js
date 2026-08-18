4a3b4af2451d114b93b111206e83a515fa7c6e53
Matrix AI Network <30994189+MatrixAINetworkMan@users.noreply.github.com>
2026-08-18 22:46:24 +0800

    Create chainid‑4959.js

---
+++ constants/additionalChainRegistry/chainid‑4959.js
@@ -0,0 +1,28 @@
+export default {
+  "name": "Matrix AI Network",
+  "chain": "MATRIX",
+  "rpc": [
+    "https://rpc.matrix.io"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Matrix AI Network",
+    "symbol": "MAN",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://matrix.io",
+  "shortName": "MATRIX",
+  "chainId": 4959,
+  "networkId": 4959,
+  "explorers": [
+    {
+      "name": "Matrix BlockScout",
+      "url": "https://scan.matrix.io",
+      "standard": "EIP3091"
+    }
+  ]
+}
