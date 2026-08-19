edc8f41978a7ef50898cb2f453f44f7867b5f030
codingwithmanny <manny@berachain.com>
2026-08-19 17:31:47 +0300

    feat: 🎸 Added Berachain Chains & Block Explorers

---
+++ constants/additionalChainRegistry/chainid-80069.js
@@ -0,0 +1,27 @@
+export const data = {
+  "name": "Berachain Bepolia",
+  "chain": "Berachain Bepolia",
+  "rpc": [
+    "https://bepolia.rpc.berachain.com"
+  ],
+  "faucets": [
+    "https://bepolia.faucet.berachain.com"
+  ],
+  "nativeCurrency": {
+    "name": "Bera",
+    "symbol": "BERA",
+    "decimals": 18
+  },
+  "infoURL": "https://berachain.com",
+  "shortName": "berachain-bepolia",
+  "chainId": 80069,
+  "networkId": 80069,
+  "explorers": [
+    {
+      "name": "Berascan Berachain Bepolia Testnet Block Explorer",
+      "url": "https://testnet.berascan.com",
+      "standard": "EIP3091"
+    }
+  ],
+  "icon": "berachain"
+}
