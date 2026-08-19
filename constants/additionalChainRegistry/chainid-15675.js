f1dbc5ad8770b6ad35ec76c5084d24af6a8ba577
holodao <hello@holodao.ai>
2026-08-19 09:46:06 +0800

    Add holodao Mainnet and Testnet (ChainId 15675,15676)

---
+++ constants/additionalChainRegistry/chainid-15675.js
@@ -0,0 +1,25 @@
+{
+  "name": "HOLODAO Mainnet",
+  "chain": "HOLODAO",
+  "rpc": [
+    "https://rpc.holodao.ai",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "HOLO COIN",
+    "symbol": "HOLO",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://holodao.ai",
+  "shortName": "holo",
+  "chainId": 15675,
+  "networkId": 1,
+  "icon": "holo",
+  "explorers": [{
+    "name": "holoscan",
+    "url": "https://holoscan.holodao.ai",
+    "icon": "holoscan",
+    "standard": ""
+  }]
+}
