d3a6d21f37695722e2da254efe731b489e0ed1b8
bimbochainnetwork-gif <bimbochainnetwork@gmail.com>
2026-08-24 16:30:32 +0300

    Create chainid-15497.js

---
+++ constants/additionalChainRegistry/chainid-15497.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "name": "Bimbo Chain",
+  "chain": "BMC",
+  "rpc": [
+    "https://bimborpc.com"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Bimbo Coin",
+    "symbol": "BMC",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://bimbochain.com",
+  "shortName": "bimbo",
+  "chainId": 15497,
+  "networkId": 15497,
+  "explorers": [
+    {
+      "name": "bimboscan",
+      "url": "https://bimboscan.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
