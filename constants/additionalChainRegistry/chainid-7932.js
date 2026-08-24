7227712ed4b63f7e91bd8c4c97c4b8a7c8088ad8
Sumon7932 <sumonislam7932@gmail.com>
2026-08-24 13:04:04 +0600

    Add Univex Network chain configuration

---
+++ constants/additionalChainRegistry/chainid-7932.js
@@ -0,0 +1,29 @@
+{
+  "name": "Univex Network",
+  "chain": "UNIVEX",
+  "rpc": [
+    "https://rpc.univexnetwork.com"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Univex",
+    "symbol": "UNIVEX",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://project.univexnetwork.xyz",
+  "shortName": "univex",
+  "chainId": 7932,
+  "networkId": 7932,
+  "icon": "univex",
+  "explorers": [
+    {
+      "name": "Univex Explorer",
+      "url": "https://explorer.univexnetwork.com",
+      "standard": "EIP3091"
+    }
+  ]
+}
