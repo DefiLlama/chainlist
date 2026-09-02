85dd0d42fc80f0584cac7347b5e311bbaf1b19f8
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2026-09-02 17:46:08 -0400

    Add Emberchain configuration for chain ID 7773

---
+++ constants/additionalChainRegistry/chainid-7773.js
@@ -0,0 +1,29 @@
+{
+  "name": "Emberchain",
+  "chain": "EMBR",
+  "rpc": [
+    "https://emberchain.org/api/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Ember",
+    "symbol": "EMBR",
+    "decimals": 18
+  },
+  "features": [
+    { "name": "EIP155" }
+  ],
+  "infoURL": "https://emberchain.org",
+  "shortName": "embr",
+  "chainId": 7773,
+  "networkId": 7773,
+  "icon": "https://emberchain.org/ember-coin.svg",
+  "explorers": [
+    {
+      "name": "Emberchain Explorer",
+      "url": "https://emberchain.org/ledger",
+      "icon": "https://emberchain.org/ember-coin.svg",
+      "standard": "EIP3091"
+    }
+  ]
+}
