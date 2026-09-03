96339ba8a1f0dfdcf68cd91d4e3781829417965c
Wind Pay <windpayxyz@gmail.com>
2026-09-04 01:01:25 +0600

    Add files via upload

---
+++ chainid5552020.js
@@ -0,0 +1,25 @@
+{
+  "name": "Windpay Mainnet",
+  "chain": "WIND",
+  "rpc": [
+    "https://rpc.windpay.org/rpc",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Windpay",
+    "symbol": "WIND",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://windpay.org",
+  "shortName": "WIND",
+  "chainId": 5552020,
+  "networkId": 5552020,
+  "icon": "https://github.com/xwindpay/logo/blob/main/photo_2026-09-01_23-52-06%20(2).jpg",
+  "explorers": [{
+    "name": "windpayscan",
+    "url": "https://explorer.windpay.org",
+    "icon": "windpay",
+    "standard": "EIP3091"
+  }]
+}
