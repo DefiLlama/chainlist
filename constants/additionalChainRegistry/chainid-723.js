9d7343319fbab3341df99f449a98d03693ea2daa
Demon723 <68183122+Demon723@users.noreply.github.com>
2026-09-08 02:31:11 +0530

    Add chainid-723.js for LXON blockchain configuration
    Adding LXON blockchain network to Chainlist.
    
    Network Details:
    - Chain Name: LXON
    - Chain ID: 723
    - RPC URL: https://www.lxon.in
    - Native Currency: XON
    - Decimals: 18
    - Network Type: EVM-compatible
    
    The LXON blockchain is a production-ready network running on AWS with 4 nodes (1 RPC + 3 validators). The network features 1-second block time and is currently operational at https://www.lxon.in
    
    This network is ready for public use and MetaMask integration.

---
+++ constants/additionalChainRegistry/chainid-723.js
@@ -0,0 +1,18 @@
+{
+  "name": "LXON",
+  "chain": "LXON",
+  "rpc": ["https://www.lxon.in"],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "LXON",
+    "symbol": "XON",
+    "decimals": 18
+  },
+  "features": [{"name": "EIP155"}],
+  "infoURL": "https://www.lxon.in",
+  "shortName": "lxon",
+  "chainId": 723,
+  "networkId": 723,
+  "icon": "lxon",
+  "explorers": []
+}
