2caf42f3a9a2b1b4a0d889a916a761fd7d72c1df
VORDIUM <104957254+Vordium@users.noreply.github.com>
2026-09-09 01:54:56 +0400

    Add Vordium chain data to additionalChainRegistry

---
+++ constants/additionalChainRegistry/chainid-101101.js
@@ -0,0 +1,24 @@
+export const data ={
+  "name": "Vordium",
+  "chain": "VORD",
+  "rpc": [
+    "https://rpc.vordium.com"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "Vordium",
+    "symbol": "VORD",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }],
+  "infoURL": "https://vordium.com",
+  "shortName": "vord",
+  "chainId": 101101,
+  "networkId": 101101,
+  "icon": "vordium",
+  "explorers": [{
+    "name": "vordscan",
+    "url": "https://vordscan.io",
+    "standard": "EIP3091"
+  }]
+}
