e852566aedde1fa92c8ae88ee75d6efa1804ee6a
SR_TECH <69189392+King11919@users.noreply.github.com>
2026-09-16 15:50:34 +0530

    Add NAKAM Chain (Chain ID: 11941) (#3134)
    * Add Entropy Chain registry data
    
    * Update chainid-19911.js
    
    * Add MON CHAIN registry data
    
    * Add Zentra privacy chain configuration
    
    * Add JOA Chain registry data
    
    * Delete constants/additionalChainRegistry/chainid-1191
    
    * Add JOA Chain configuration
    
    * Delete constants/additionalChainRegistry/zentra.js
    
    * Add files via upload
    
    * Add new file chainid-4848.js
    
    * Update chainid-4848.js
    
    * Add BlockSurety chain data for chainId 4848
    
    * Delete chainid-4848.js
    
    * Add NAKAM Chain data to additionalChainRegistry

---
+++ constants/additionalChainRegistry/chainid-11941.js
@@ -0,0 +1,24 @@
+export const data = {
+  "chainId": 11941,
+  "name": "NAKAM Chain",
+  "shortName": "nkm",
+  "chain": "NKM",
+  "networkId": 11941,
+  "infoURL": "https://nakamcoin.com",
+  "rpc": ["https://rpc.nakamcoin.com"],
+  "nativeCurrency": {
+    "name": "NAKAM",
+    "symbol": "NKM",
+    "decimals": 18
+  },
+  "explorers": [
+    {
+      "name": "NAKAM Explorer",
+      "url": "https://explorer.nakamcoin.com",
+      "standard": "EIP3091"
+    }
+  ],
+  "faucets": [],
+  "features": ["EVM Compatible", "MetaMask Supported"],
+  "icon": ""
+};
