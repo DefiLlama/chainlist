c8ae71da3991ba88ca85a86b8aa664a83642aa7c
actumic-dev <actumic@gmail.com>
2026-09-19 20:07:00 +0700

    feat: add Actumic Chain 402 (chainId 402)

---
+++ constants/additionalChainRegistry/chainid-402.js
@@ -0,0 +1,31 @@
+export const data = {
+  "name": "Actumic",
+  "chain": "ACTUMIC",
+  "rpc": [
+    "https://scan.actumic.com/rpc"
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "USD Coin",
+    "symbol": "USDC",
+    "decimals": 6
+  },
+  "features": [
+    { "name": "EIP155" },
+    { "name": "EIP1559" }
+  ],
+  "infoURL": "https://actumic.com",
+  "shortName": "actumic",
+  "chainId": 402,
+  "networkId": 402,
+  "icon": "actumic",
+  "explorers": [
+    {
+      "name": "Actumic Explorer",
+      "url": "https://scan.actumic.com",
+      "icon": "actumic",
+      "standard": "EIP3091"
+    }
+  ],
+  "status": "active"
+};
