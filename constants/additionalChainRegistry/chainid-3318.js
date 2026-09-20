43b8b932435f455466fde706ba991fa6a2e772b2
Vorachain <admin@vorachain.com>
2026-09-20 20:11:46 +0700

    Add VORA Chain (chainId 3318)

---
+++ constants/additionalChainRegistry/chainid-3318.js
@@ -0,0 +1,24 @@
+export const data = {
+  name: "VORA Chain",
+  chain: "VORA",
+  icon: "vora",
+  rpc: ["https://vorascan.io/rpc"],
+  faucets: [],
+  nativeCurrency: {
+    name: "VORA",
+    symbol: "VRA",
+    decimals: 18,
+  },
+  features: [{ name: "EIP155" }, { name: "EIP1559" }],
+  infoURL: "https://vorachain.com",
+  shortName: "vora",
+  chainId: 3318,
+  networkId: 3318,
+  explorers: [
+    {
+      name: "vorascan",
+      url: "https://vorascan.io",
+      standard: "EIP3091",
+    },
+  ],
+};
