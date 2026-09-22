a64f20bec6997b3ce75c08128c2a1dbe914a47a5
fmrmf <m@fmrmf.xyz>
2026-09-22 15:55:14 -0400

    chore: chainid-99801 elysium testnet js file

---
+++ constants/additionalChainRegistry/chainid-99801.js
@@ -0,0 +1,23 @@
+export const data = {
+  name: "Elysium Testnet",
+  chain: "Elysium Testnet",
+  icon: "https://elysium.kinetiq.xyz/assets/brand/elysium/elysium-logomark-colorized.png",
+  rpc: ["https://testnet-rpc.elysium.kinetiq.xyz"],
+  faucets: [],
+  nativeCurrency: {
+    name: "HYPE",
+    symbol: "HYPE",
+    decimals: 18,
+  },
+  infoURL: "https://elysium.kinetiq.xyz/docs/what-is-elysium",
+  shortName: "elysium-testnet",
+  chainId: 99801,
+  networkId: 99801,
+  explorers: [
+    {
+      name: "Elysium Testnet Explorer",
+      url: "https://elysium.kinetiq.xyz/testnet-explorer"
+    },
+  ],
+  status: "active",
+};
