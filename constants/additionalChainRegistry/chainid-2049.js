cc0feb21af500aeaa8d54cc6219015f0392952eb
pikocoin-lab <pikocoin-lab@users.noreply.github.com>
2026-09-25 19:23:59 +0800

    Add PikoChain (chainId 2049)

---
+++ constants/additionalChainRegistry/chainid-2049.js
@@ -0,0 +1,26 @@
+export const data = {
+    "name": "PikoChain",
+    "chain": "PIKO",
+    "rpc": [
+      "https://pikochain.serveousercontent.com"
+    ],
+    "faucets": [
+      "https://pikochain.serveousercontent.com/faucet.html"
+    ],
+    "nativeCurrency": {
+      "name": "Piko",
+      "symbol": "PIKO",
+      "decimals": 18
+    },
+    "features": [{ "name": "EIP155" }],
+    "infoURL": "https://github.com/pikocoin-lab/pikochain",
+    "shortName": "piko",
+    "chainId": 2049,
+    "networkId": 2049,
+    "explorers": [
+      {
+        "name": "PikoChain Explorer",
+        "url": "https://pikochain.serveousercontent.com/explorer.html"
+      }
+    ]
+  }
